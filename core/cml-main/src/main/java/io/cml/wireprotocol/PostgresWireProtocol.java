ccdc6bcd4fbe291be488beb07c76a45350b5ee8c
Jia-Xuan Liu <liugs963@gmail.com>
2022-08-04 10:24:20 +0800

    rename variable

--- core/cml-main/src/main/java/io/cml/wireprotocol/PostgresWireProtocol.java
+++ core/cml-main/src/main/java/io/cml/wireprotocol/PostgresWireProtocol.java
@@ -11,6 +11,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.cml.wireprotocol;
 
 import com.google.common.collect.ImmutableList;
@@ -463,12 +477,12 @@ public class PostgresWireProtocol
                 portal.setResultSetSender(connectorRecordIterable.get());
             }
 
-            ConnectorRecordIterator hiveRecordIterable = portal.getConnectorRecordIterable();
+            ConnectorRecordIterator connectorRecordIterable = portal.getConnectorRecordIterable();
             FormatCodes.FormatCode[] resultFormatCodes = wireProtocolSession.getResultFormatCodes(portalName);
             ResultSetSender resultSetSender = new ResultSetSender(
                     statement,
                     channel,
-                    hiveRecordIterable,
+                    connectorRecordIterable,
                     maxRows,
                     portal.getRowCount(),
                     resultFormatCodes);
