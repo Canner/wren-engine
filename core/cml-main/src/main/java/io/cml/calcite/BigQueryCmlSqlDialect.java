3bd12629e6c3332f6022db414963cc814f562277
Jia-Xuan Liu <liugs963@gmail.com>
2022-08-26 10:24:52 +0800

    Support access array element for big query (#105)

--- core/cml-main/src/main/java/io/cml/calcite/BigQueryCmlSqlDialect.java
+++ core/cml-main/src/main/java/io/cml/calcite/BigQueryCmlSqlDialect.java
@@ -18,10 +18,13 @@ import io.airlift.log.Logger;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.sql.SqlAlienSystemTypeNameSpec;
 import org.apache.calcite.sql.SqlBasicCall;
+import org.apache.calcite.sql.SqlCall;
 import org.apache.calcite.sql.SqlDataTypeSpec;
+import org.apache.calcite.sql.SqlKind;
 import org.apache.calcite.sql.SqlLiteral;
 import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.sql.SqlNodeList;
+import org.apache.calcite.sql.SqlWriter;
 import org.apache.calcite.sql.dialect.BigQuerySqlDialect;
 import org.apache.calcite.sql.fun.SqlCase;
 import org.apache.calcite.sql.fun.SqlStdOperatorTable;
@@ -141,5 +144,26 @@ public class BigQueryCmlSqlDialect
         SqlAlienSystemTypeNameSpec typeNameSpec = new SqlAlienSystemTypeNameSpec(
                 typeAlias, typeName, SqlParserPos.ZERO);
         return new SqlDataTypeSpec(typeNameSpec, SqlParserPos.ZERO);
+    }
+
+    @Override
+    public void unparseCall(final SqlWriter writer, final SqlCall call, final int leftPrec,
+            final int rightPrec)
+    {
+        if (call.getKind() == SqlKind.ITEM) {
+            call.operand(0).unparse(writer, leftPrec, rightPrec);
+            final SqlWriter.Frame indexFrame = writer.startList("[", "]");
+            // BigQuery doesn't support the normal way to access array element like `ARRAY[1,2,3][1]`.
+            // It should use `ORDINAL` or `OFFSET` operator to handle index value.
+            // Since pg is 1-based array index, that's why we use `ORDINAL` here.
+            // https://cloud.google.com/bigquery/docs/reference/standard-sql/arrays#accessing_array_elements
+            final SqlWriter.Frame funcFrame = writer.startFunCall("ORDINAL");
+            call.operand(1).unparse(writer, leftPrec, rightPrec);
+            writer.endFunCall(funcFrame);
+            writer.endList(indexFrame);
+        }
+        else {
+            super.unparseCall(writer, call, leftPrec, rightPrec);
+        }
     }
 }
