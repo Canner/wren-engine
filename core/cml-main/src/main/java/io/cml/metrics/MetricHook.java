ba5ed215f54f8f4ccdd07abb0f1979a87510cfb1
Jia-Xuan Liu <liugs963@gmail.com>
2022-09-08 17:52:10 +0800

    implement MetricHook and add test

---
+++ core/cml-main/src/main/java/io/cml/metrics/MetricHook.java
@@ -0,0 +1,106 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.cml.metrics;
+
+import io.cml.metadata.Metadata;
+import io.cml.spi.CmlException;
+import io.cml.spi.metadata.SchemaTableName;
+
+import javax.inject.Inject;
+
+import java.util.List;
+
+import static io.cml.spi.metadata.StandardErrorCode.ALREADY_EXISTS;
+import static io.cml.spi.metadata.StandardErrorCode.NOT_FOUND;
+import static java.lang.String.format;
+import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toUnmodifiableList;
+
+public class MetricHook
+{
+    private final MetricStore metricStore;
+    private final Metadata metadata;
+
+    @Inject
+    public MetricHook(
+            MetricStore metricStore,
+            Metadata metadata)
+    {
+        this.metricStore = requireNonNull(metricStore, "metricStore is null");
+        this.metadata = requireNonNull(metadata, "metadata is null");
+    }
+
+    public void handleCreate(Metric metric)
+    {
+        if (metricStore.getMetric(metric.getName()).isPresent()) {
+            throw new CmlException(ALREADY_EXISTS, "metric %s already exist");
+        }
+
+        createMetric(metric);
+    }
+
+    private void createMetric(Metric metric)
+    {
+        List<MetricSql> createdMetricSqls = MetricSql.of(metric).stream()
+                .map(this::createRemoteMaterializedView)
+                .collect(toUnmodifiableList());
+
+        metricStore.createMetric(metric);
+        createdMetricSqls.forEach(metricStore::createMetricSql);
+    }
+
+    private MetricSql createRemoteMaterializedView(MetricSql metricSql)
+    {
+        SchemaTableName schemaTableName = new SchemaTableName(metadata.getMaterializedViewSchema(), metricSql.getName());
+        try {
+            metadata.createMaterializedView(schemaTableName, metricSql.sql());
+            return metricSql.withStatus(MetricSql.Status.SUCCESS, null);
+        }
+        catch (Exception e) {
+            return metricSql.withStatus(MetricSql.Status.FAILED, e.getMessage());
+        }
+    }
+
+    public void handleUpdate(Metric metric)
+    {
+        if (metricStore.getMetric(metric.getName()).isEmpty()) {
+            throw new CmlException(NOT_FOUND, format("metric %s is not found", metric.getName()));
+        }
+
+        // TODO: concurrency accessing issue for query, update and delete
+        //  https://github.com/Canner/canner-metric-layer/issues/113
+        dropMetric(metric);
+        createMetric(metric);
+    }
+
+    private void dropMetric(Metric metric)
+    {
+        List<MetricSql> metricSqls = metricStore.listMetricSqls(metric.getName());
+        metricSqls.stream()
+                .map(metricSql -> new SchemaTableName(metadata.getMaterializedViewSchema(), metricSql.getName()))
+                .forEach(metadata::deleteMaterializedView);
+
+        metricStore.dropMetric(metric.getName());
+    }
+
+    public void handleDrop(Metric metric)
+    {
+        if (metricStore.getMetric(metric.getName()).isEmpty()) {
+            throw new CmlException(NOT_FOUND, format("metric %s is not found", metric.getName()));
+        }
+
+        dropMetric(metric);
+    }
+}
---
+++ core/cml-testing/src/test/java/io/cml/testing/bigquery/TestFileMetricHookWithBigQuery.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.cml.testing.bigquery;
+
+import io.cml.connector.bigquery.BigQueryClient;
+import io.cml.connector.bigquery.BigQueryConfig;
+import io.cml.connector.bigquery.BigQueryMetadata;
+import io.cml.connector.bigquery.BigQuerySqlConverter;
+import io.cml.metadata.Metadata;
+import io.cml.metrics.FileMetricStore;
+import io.cml.metrics.MetricHook;
+import io.cml.metrics.MetricStore;
+import io.cml.server.module.BigQueryConnectorModule;
+import io.cml.spi.metadata.SchemaTableName;
+import io.cml.sql.SqlConverter;
+import io.cml.testing.AbstractTestMetricHook;
+
+import java.nio.file.Path;
+import java.util.List;
+
+import static java.lang.System.getenv;
+import static java.util.Objects.requireNonNull;
+
+public class TestFileMetricHookWithBigQuery
+        extends AbstractTestMetricHook
+{
+    private final BigQueryClient bigQueryClient;
+    private final Metadata metadata;
+    private final MetricStore metricStore;
+    private final SqlConverter sqlConverter;
+    private final MetricHook metricHook;
+
+    public TestFileMetricHookWithBigQuery()
+    {
+        BigQueryConfig config = new BigQueryConfig();
+        config.setProjectId(getenv("TEST_BIG_QUERY_PROJECT_ID"))
+                .setParentProjectId(getenv("TEST_BIG_QUERY_PARENT_PROJECT_ID"))
+                .setCredentialsKey(getenv("TEST_BIG_QUERY_CREDENTIALS_BASE64_JSON"))
+                .setLocation("asia-east1");
+
+        bigQueryClient = BigQueryConnectorModule.provideBigQuery(
+                config,
+                BigQueryConnectorModule.createHeaderProvider(),
+                BigQueryConnectorModule.provideBigQueryCredentialsSupplier(config));
+
+        this.metadata = new BigQueryMetadata(bigQueryClient, config);
+        this.metricStore = new FileMetricStore(Path.of(requireNonNull(getenv("TEST_CML_FILE_METRIC_STORE_HOME"))));
+        this.metricHook = new MetricHook(metricStore, metadata);
+        this.sqlConverter = new BigQuerySqlConverter(metadata);
+    }
+
+    @Override
+    public MetricStore getMetricStore()
+    {
+        return metricStore;
+    }
+
+    @Override
+    public Metadata getMetadata()
+    {
+        return metadata;
+    }
+
+    @Override
+    public SqlConverter getSqlConverter()
+    {
+        return sqlConverter;
+    }
+
+    @Override
+    public MetricHook getMetricHook()
+    {
+        return metricHook;
+    }
+
+    @Override
+    public void dropTables(List<SchemaTableName> createdTables)
+    {
+        createdTables.forEach(bigQueryClient::dropTable);
+    }
+}
