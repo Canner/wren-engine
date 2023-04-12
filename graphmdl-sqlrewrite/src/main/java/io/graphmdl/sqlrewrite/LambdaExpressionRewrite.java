1b62e7e634376f03e144d07077ab0a5a061c8ad0
Jia-Xuan Liu <liugs963@gmail.com>
2023-04-12 11:19:34 +0800

    support lambda expression rewrite basiclly

---
+++ graphmdl-sqlrewrite/src/main/java/io/graphmdl/sqlrewrite/LambdaExpressionRewrite.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.graphmdl.sqlrewrite;
+
+import io.graphmdl.sqlrewrite.analyzer.Field;
+import io.trino.sql.tree.AstVisitor;
+import io.trino.sql.tree.DereferenceExpression;
+import io.trino.sql.tree.Expression;
+import io.trino.sql.tree.Identifier;
+import io.trino.sql.tree.Node;
+import io.trino.sql.tree.StringLiteral;
+import io.trino.sql.tree.SubscriptExpression;
+
+import java.util.Optional;
+
+import static io.graphmdl.sqlrewrite.RelationshipCteGenerator.TARGET_REFERENCE;
+import static java.util.Objects.requireNonNull;
+
+public class LambdaExpressionRewrite
+{
+    public static Expression rewrite(Node node, Field baseField, Identifier argument)
+    {
+        return (Expression) new Visitor(baseField, argument).process(node, Optional.empty());
+    }
+
+    static class Visitor
+            extends AstVisitor<Node, Optional<Node>>
+    {
+        private final Field baseField;
+        private final Identifier argument;
+
+        Visitor(Field baseField, Identifier argument)
+        {
+            this.baseField = requireNonNull(baseField, "baseField is null");
+            this.argument = requireNonNull(argument, "argument is null");
+        }
+
+        @Override
+        protected Node visitDereferenceExpression(DereferenceExpression node, Optional<Node> context)
+        {
+            if (node.getBase() instanceof Identifier) {
+                return process(node.getBase(), Optional.ofNullable(node.getField()));
+            }
+            return new DereferenceExpression((Expression) process(node.getBase(), Optional.ofNullable(node.getField())), node.getField());
+        }
+
+        @Override
+        protected Node visitSubscriptExpression(SubscriptExpression node, Optional<Node> context)
+        {
+            if (node.getBase() instanceof DereferenceExpression) {
+                return new SubscriptExpression((Expression) process(node.getBase(),
+                        Optional.ofNullable((((DereferenceExpression) node.getBase()).getBase()))),
+                        node.getIndex());
+            }
+            return new SubscriptExpression((Expression) process(node.getBase(), Optional.empty()), node.getIndex());
+        }
+
+        @Override
+        protected Node visitIdentifier(Identifier node, Optional<Node> context)
+        {
+            if (context.isEmpty()) {
+                return new StringLiteral(String.format("Relationship<%s>", baseField.getModelName().getSchemaTableName().getTableName()));
+            }
+            if (argument.equals(node)) {
+                return new DereferenceExpression(new Identifier(TARGET_REFERENCE), (Identifier) context.get());
+            }
+            return node;
+        }
+    }
+}
---
+++ graphmdl-sqlrewrite/src/test/java/io/graphmdl/sqlrewrite/TestLambdaExpressionRewrite.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.graphmdl.sqlrewrite;
+
+import io.graphmdl.base.CatalogSchemaTableName;
+import io.graphmdl.sqlrewrite.analyzer.Field;
+import io.trino.sql.parser.ParsingOptions;
+import io.trino.sql.parser.SqlParser;
+import io.trino.sql.tree.Expression;
+import io.trino.sql.tree.Identifier;
+import io.trino.sql.tree.Node;
+import io.trino.sql.tree.QualifiedName;
+import io.trino.sql.tree.Query;
+import io.trino.sql.tree.QuerySpecification;
+import io.trino.sql.tree.SingleColumn;
+import io.trino.sql.tree.Statement;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static io.trino.sql.parser.ParsingOptions.DecimalLiteralTreatment.AS_DECIMAL;
+
+public class TestLambdaExpressionRewrite
+{
+    private final static SqlParser SQL_PARSER = new SqlParser();
+
+    @DataProvider
+    public Object[][] lambdaExpression()
+    {
+        // TODO: support FunctionCall, ComparisonExpression ...
+        return new Object[][] {
+                {"book.f1.f2.f3"},
+                {"book"},
+                {"book.f1.a1[1].f2"}
+        };
+    }
+
+    @Test(dataProvider = "lambdaExpression")
+    public void testLambdaExpressionRewrite(String sql)
+    {
+        CatalogSchemaTableName catalogSchemaTableName = new CatalogSchemaTableName("graphmdl", "test", "Book");
+        Node node = LambdaExpressionRewrite.rewrite(getSelectItem(String.format("select %s", sql)),
+                Field.builder()
+                        .isRelationship(true)
+                        .modelName(catalogSchemaTableName)
+                        .columnName("books")
+                        .name("books")
+                        .relationAlias(QualifiedName.of("t"))
+                        .build(), new Identifier("book"));
+        System.out.println(node);
+    }
+
+    private Expression getSelectItem(String sql)
+    {
+        Statement statement = SQL_PARSER.createStatement(sql, new ParsingOptions(AS_DECIMAL));
+        return ((SingleColumn) ((QuerySpecification) ((Query) statement).getQueryBody()).getSelect().getSelectItems().get(0)).getExpression();
+    }
+}
