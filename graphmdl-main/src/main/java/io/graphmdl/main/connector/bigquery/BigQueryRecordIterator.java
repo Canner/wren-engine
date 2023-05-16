7ba242171d49dce462fc556afa5f3943f1b9e266
Wen-Hsiu Chang <aa8693@gmail.com>
2023-05-16 09:50:09 +0800

    Modify interval type output format as period

--- graphmdl-main/src/main/java/io/graphmdl/main/connector/bigquery/BigQueryRecordIterator.java
+++ graphmdl-main/src/main/java/io/graphmdl/main/connector/bigquery/BigQueryRecordIterator.java
@@ -23,12 +23,18 @@ import io.graphmdl.base.ConnectorRecordIterator;
 import io.graphmdl.base.type.PGType;
 import io.graphmdl.base.type.PGTypes;
 import io.graphmdl.connector.bigquery.BigQueryType;
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.Period;
 
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
+import static java.lang.String.format;
+import static java.util.Locale.ENGLISH;
 import static java.util.Objects.requireNonNull;
 
 public class BigQueryRecordIterator
@@ -117,6 +123,8 @@ public class BigQueryRecordIterator
             case NUMERIC:
             case BIGNUMERIC:
                 return fieldValue.getNumericValue();
+            case INTERVAL:
+                return convertBigQueryIntervalToPeriod(fieldValue.getStringValue());
             default:
                 throw new IllegalArgumentException("Unsupported type: " + typeName);
         }
@@ -125,5 +133,28 @@ public class BigQueryRecordIterator
     public List<PGType> getTypes()
     {
         return types;
+    }
+
+    private Period convertBigQueryIntervalToPeriod(String value)
+    {
+        // BigQuery interval format: [sign]Y-M [sign]D [sign]H:M:S[.F], and F up to six digits
+        Pattern pattern = Pattern.compile("(?<NEG>-?)(?<Y>[0-9]+)-(?<M>[0-9]+) (?<D>-?[0-9]+) (?<NEGTIME>-?)(?<H>[0-9]+):(?<MIN>[0-9]+):(?<S>[0-9]+).?(?<F>[0-9]{1,6})?");
+        Matcher matcher = pattern.matcher(value);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(format(ENGLISH, "Invalid interval format: %s", value));
+        }
+        int sign = matcher.group("NEG").isEmpty() ? 1 : -1;
+        int year = Integer.parseInt(matcher.group("Y")) * sign;
+        int mon = Integer.parseInt(matcher.group("M")) * sign;
+        int day = Integer.parseInt(matcher.group("D"));
+        int signTime = matcher.group("NEGTIME").isEmpty() ? 1 : -1;
+        int hour = Integer.parseInt(matcher.group("H")) * signTime;
+        int min = Integer.parseInt(matcher.group("MIN")) * signTime;
+        int sec = Integer.parseInt(matcher.group("S")) * signTime;
+        if (matcher.group("F") == null) {
+            return new Period(year, mon, 0, day, hour, min, sec, 0);
+        }
+        String micro = StringUtils.rightPad(matcher.group("F"), 6, '0');
+        return new Period(year, mon, 0, day, hour, min, sec, Integer.parseInt(micro) / 1000);
     }
 }
