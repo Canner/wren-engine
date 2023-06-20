f9b4177f133287ca4d1edda8d19c1cd9e68efa21
Kuan-Po Tseng <brandboat@gmail.com>
2023-06-20 15:33:50 +0800

    Support first function (#281)

--- graphmdl-sqlrewrite/src/test/java/io/graphmdl/sqlrewrite/TestGraphMDLFunctionSqlRewrite.java
+++ graphmdl-sqlrewrite/src/test/java/io/graphmdl/sqlrewrite/TestGraphMDLFunctionSqlRewrite.java
@@ -17,12 +17,12 @@ package io.graphmdl.sqlrewrite;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import static io.graphmdl.sqlrewrite.AnyFunctionSqlRewrite.ANY_FUNCTION_SQL_REWRITE;
+import static io.graphmdl.sqlrewrite.GraphMDLFunctionSqlRewrite.GRAPHMDL_FUNCTION_SQL_REWRITE;
 import static io.graphmdl.sqlrewrite.Utils.parseSql;
 import static io.trino.sql.SqlFormatter.formatSql;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class TestAnyFunctionSqlRewrite
+public class TestGraphMDLFunctionSqlRewrite
 {
     @DataProvider
     public Object[][] rewrite()
@@ -34,14 +34,17 @@ public class TestAnyFunctionSqlRewrite
                 {"SELECT any(filter(rs, rs -> rs.col1 = 'F')) AS a FROM model", "SELECT filter(rs, rs -> rs.col1 = 'F')[1] AS a FROM model"},
                 {"SELECT any(filter(rs, rs -> rs.col1 = 'F')).col1 FROM model", "SELECT filter(rs, rs -> rs.col1 = 'F')[1].col1 FROM model"},
                 {"SELECT any(filter(rs, rs -> rs.col1 = 'F')).col1 AS a FROM model", "SELECT filter(rs, rs -> rs.col1 = 'F')[1].col1 AS a FROM model"},
-                {"SELECT concat(any(filter(rs, rs -> rs.col1 = 'F')).col1, 'foo') AS a FROM model", "SELECT concat(filter(rs, rs -> rs.col1 = 'F')[1].col1, 'foo') AS a FROM model"},
+                {"SELECT concat(any(filter(rs, rs -> rs.col1 = 'F')).col1, 'foo') AS a FROM model",
+                        "SELECT concat(filter(rs, rs -> rs.col1 = 'F')[1].col1, 'foo') AS a FROM model"},
+                {"SELECT first(filter(rs, rs -> rs.col1 = 'F'), col, ASC).col1 AS a FROM model",
+                        "SELECT array_sort(filter(rs, rs -> rs.col1 = 'F'), col, ASC)[1].col1 AS a FROM model"},
         };
     }
 
     @Test(dataProvider = "rewrite")
     public void testSqlRewrite(String original, String expected)
     {
-        assertThat(formatSql(ANY_FUNCTION_SQL_REWRITE.apply(parseSql(original), null, null)))
+        assertThat(formatSql(GRAPHMDL_FUNCTION_SQL_REWRITE.apply(parseSql(original), null, null)))
                 .isEqualTo(formatSql(parseSql(expected)));
     }
 }
