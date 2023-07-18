54807c81ec132ef28d327f619982eb4415f7bf9c
Wen-Hsiu Chang <aa8693@gmail.com>
2023-07-18 17:08:10 +0800

    Fix pg bytea type

--- accio-main/src/main/java/io/accio/main/connector/postgres/PostgresSqlConverter.java
+++ accio-main/src/main/java/io/accio/main/connector/postgres/PostgresSqlConverter.java
@@ -14,29 +14,49 @@
 
 package io.accio.main.connector.postgres;
 
+import com.google.common.collect.ImmutableList;
 import io.accio.base.SessionContext;
 import io.accio.base.sql.SqlConverter;
+import io.accio.main.metadata.Metadata;
+import io.accio.main.sql.SqlRewrite;
+import io.accio.main.sql.postgres.RewriteToPostgresType;
 import io.airlift.log.Logger;
 import io.trino.sql.tree.Node;
 
 import javax.inject.Inject;
 
+import java.util.List;
+
 import static io.accio.sqlrewrite.Utils.parseSql;
 import static io.trino.sql.SqlFormatter.Dialect.POSTGRES;
 import static io.trino.sql.SqlFormatter.formatSql;
+import static java.util.Objects.requireNonNull;
 
 public class PostgresSqlConverter
         implements SqlConverter
 {
     private static final Logger LOG = Logger.get(PostgresSqlConverter.class);
+    private final Metadata metadata;
 
     @Inject
-    public PostgresSqlConverter() {}
+    public PostgresSqlConverter(Metadata metadata)
+    {
+        this.metadata = requireNonNull(metadata, "metadata is null");
+    }
 
     @Override
     public String convert(String sql, SessionContext sessionContext)
     {
         Node rewrittenNode = parseSql(sql);
+        List<SqlRewrite> sqlRewrites = ImmutableList.of(RewriteToPostgresType.INSTANCE);
+        LOG.info("[Input sql]: %s", sql);
+
+        for (SqlRewrite rewrite : sqlRewrites) {
+            LOG.debug("Before %s: %s", rewrite.getClass().getSimpleName(), formatSql(rewrittenNode));
+            rewrittenNode = rewrite.rewrite(rewrittenNode, metadata);
+            LOG.debug("After %s: %s", rewrite.getClass().getSimpleName(), formatSql(rewrittenNode));
+        }
+
         String dialectSql = formatSql(rewrittenNode, POSTGRES);
         LOG.info("[Dialect sql]: %s", dialectSql);
         return dialectSql;
