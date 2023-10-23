2a3e1362fb5742b648dc420c2793fde252c9de84
brandboat <brandboat@gmail.com>
2023-10-23 14:24:57 +0800

    Remove accio query syntax runtime support

--- accio-sqlrewrite/src/main/java/io/accio/sqlrewrite/AccioPlanner.java
+++ accio-sqlrewrite/src/main/java/io/accio/sqlrewrite/AccioPlanner.java
@@ -25,14 +25,12 @@ import java.util.List;
 
 import static io.accio.sqlrewrite.AccioSqlRewrite.ACCIO_SQL_REWRITE;
 import static io.accio.sqlrewrite.MetricViewSqlRewrite.METRIC_VIEW_SQL_REWRITE;
-import static io.accio.sqlrewrite.SyntacticSugarRewrite.SYNTACTIC_SUGAR_REWRITE;
 import static io.trino.sql.parser.ParsingOptions.DecimalLiteralTreatment.AS_DECIMAL;
 
 public class AccioPlanner
 {
     public static final List<AccioRule> ALL_RULES = List.of(
             METRIC_VIEW_SQL_REWRITE,
-            SYNTACTIC_SUGAR_REWRITE,
             ACCIO_SQL_REWRITE);
     private static final SqlParser SQL_PARSER = new SqlParser();
 
@@ -46,12 +44,10 @@ public class AccioPlanner
     public static String rewrite(String sql, SessionContext sessionContext, AccioMDL accioMDL, List<AccioRule> rules)
     {
         Statement statement = SQL_PARSER.createStatement(sql, new ParsingOptions(AS_DECIMAL));
-        Statement scopedStatement = ScopeAwareRewrite.SCOPE_AWARE_REWRITE.rewrite(statement, accioMDL, sessionContext);
-        Statement result = scopedStatement;
         for (AccioRule rule : rules) {
             // we will replace or rewrite sql node in sql rewrite, to avoid rewrite rules affect each other, format and parse sql before each rewrite
-            result = rule.apply(SQL_PARSER.createStatement(SqlFormatter.formatSql(result), new ParsingOptions(AS_DECIMAL)), sessionContext, accioMDL);
+            statement = rule.apply(SQL_PARSER.createStatement(SqlFormatter.formatSql(statement), new ParsingOptions(AS_DECIMAL)), sessionContext, accioMDL);
         }
-        return SqlFormatter.formatSql(result);
+        return SqlFormatter.formatSql(statement);
     }
 }
