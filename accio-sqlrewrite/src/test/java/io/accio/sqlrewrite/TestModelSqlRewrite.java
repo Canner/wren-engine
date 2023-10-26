1b9ffe30e6da68ca3fb069f3c79f2a5cd18f6522
brandboat <brandboat@gmail.com>
2023-10-26 14:46:44 +0800

    Enhance TestModelSqlRewrire

--- accio-sqlrewrite/src/test/java/io/accio/sqlrewrite/TestModelSqlRewrite.java
+++ accio-sqlrewrite/src/test/java/io/accio/sqlrewrite/TestModelSqlRewrite.java
@@ -15,6 +15,7 @@
 package io.accio.sqlrewrite;
 
 import io.accio.base.AccioMDL;
+import io.accio.base.dto.Manifest;
 import io.accio.testing.AbstractTestFramework;
 import io.trino.sql.parser.ParsingOptions;
 import io.trino.sql.parser.SqlParser;
@@ -41,7 +42,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class TestModelSqlRewrite
         extends AbstractTestFramework
 {
-    private static final AccioMDL ACCIOMDL = AccioMDL.fromManifest(withDefaultCatalogSchema()
+    private static final Manifest DEFAULT_MANIFEST = withDefaultCatalogSchema()
             .setRelationships(List.of(
                     relationship("WishListPeople", List.of("WishList", "People"), ONE_TO_ONE, "WishList.id = People.id"),
                     relationship("PeopleBook", List.of("People", "Book"), ONE_TO_MANY, "People.id = Book.authorId")))
@@ -74,7 +75,107 @@ public class TestModelSqlRewrite
                                     column("id", "STRING", null, false),
                                     column("bookId", "STRING", null, false)),
                             "id")))
-            .build());
+            .build();
+    private static final AccioMDL ACCIOMDL = AccioMDL.fromManifest(DEFAULT_MANIFEST);
+    @Language("SQL")
+    private static final String WITH_PEOPLE_QUERY = "" +
+            "  WishList AS (\n" +
+            "   SELECT\n" +
+            "     \"WishList\".\"id\" \"id\"\n" +
+            "   , \"WishList\".\"bookId\" \"bookId\"\n" +
+            "   FROM\n" +
+            "     (\n" +
+            "      SELECT\n" +
+            "        \"WishList\".\"id\" \"id\"\n" +
+            "      , \"WishList\".\"bookId\" \"bookId\"\n" +
+            "      FROM\n" +
+            "        (\n" +
+            "         SELECT *\n" +
+            "         FROM\n" +
+            "           table_wishlist\n" +
+            "      )  \"WishList\"\n" +
+            "   )  \"WishList\"\n" +
+            ") \n" +
+            ", People AS (\n" +
+            "   SELECT\n" +
+            "     \"People\".\"id\" \"id\"\n" +
+            "   , \"People\".\"email\" \"email\"\n" +
+            "   , \"gift\".\"gift\" \"gift\"\n" +
+            "   FROM\n" +
+            "     ((\n" +
+            "      SELECT\n" +
+            "        \"People\".\"id\" \"id\"\n" +
+            "      , \"People\".\"email\" \"email\"\n" +
+            "      FROM\n" +
+            "        (\n" +
+            "         SELECT *\n" +
+            "         FROM\n" +
+            "           table_people\n" +
+            "      )  \"People\"\n" +
+            "   )  \"People\"\n" +
+            "   LEFT JOIN (\n" +
+            "      SELECT\n" +
+            "        \"People\".\"id\"\n" +
+            "      , \"WishList\".\"bookId\" \"gift\"\n" +
+            "      FROM\n" +
+            "        ((\n" +
+            "         SELECT\n" +
+            "           id \"id\"\n" +
+            "         , id \"id\"\n" +
+            "         FROM\n" +
+            "           (\n" +
+            "            SELECT *\n" +
+            "            FROM\n" +
+            "              table_people\n" +
+            "         )  \"People\"\n" +
+            "      )  \"People\"\n" +
+            "      LEFT JOIN \"WishList\" ON (WishList.id = People.id))\n" +
+            "   )  \"gift\" ON (\"People\".\"id\" = \"gift\".\"id\"))\n" +
+            ")\n";
+
+    @Language("SQL")
+    private static final String WITH_BOOK_QUERY = WITH_PEOPLE_QUERY +
+            ", Book AS (\n" +
+            "   SELECT\n" +
+            "     \"Book\".\"bookId\" \"bookId\"\n" +
+            "   , \"Book\".\"authorId\" \"authorId\"\n" +
+            "   , \"Book\".\"publish_date\" \"publish_date\"\n" +
+            "   , \"Book\".\"publish_year\" \"publish_year\"\n" +
+            "   , \"author_gift_id\".\"author_gift_id\" \"author_gift_id\"\n" +
+            "   FROM\n" +
+            "     ((\n" +
+            "      SELECT\n" +
+            "        \"Book\".\"bookId\" \"bookId\"\n" +
+            "      , \"Book\".\"authorId\" \"authorId\"\n" +
+            "      , \"Book\".\"publish_date\" \"publish_date\"\n" +
+            "      , date_trunc('year', publish_date) \"publish_year\"\n" +
+            "      FROM\n" +
+            "        (\n" +
+            "         SELECT *\n" +
+            "         FROM\n" +
+            "           table_book\n" +
+            "      )  \"Book\"\n" +
+            "   )  \"Book\"\n" +
+            "   LEFT JOIN (\n" +
+            "      SELECT\n" +
+            "        \"Book\".\"bookId\"\n" +
+            "      , \"WishList\".\"bookId\" \"author_gift_id\"\n" +
+            "      FROM\n" +
+            "        (((\n" +
+            "         SELECT\n" +
+            "           bookId \"bookId\"\n" +
+            "         , authorId \"authorId\"\n" +
+            "         FROM\n" +
+            "           (\n" +
+            "            SELECT *\n" +
+            "            FROM\n" +
+            "              table_book\n" +
+            "         )  \"Book\"\n" +
+            "      )  \"Book\"\n" +
+            "      LEFT JOIN \"People\" ON (People.id = Book.authorId))\n" +
+            "      LEFT JOIN \"WishList\" ON (WishList.id = People.id))\n" +
+            "   )  \"author_gift_id\" ON (\"Book\".\"bookId\" = \"author_gift_id\".\"bookId\"))\n" +
+            ")\n";
 
     @Override
     protected void prepareData()
@@ -108,117 +209,19 @@ public class TestModelSqlRewrite
     @Test
     public void testModelRewrite()
     {
-        @Language("SQL") String withPeopleQuery = "" +
-                "  WishList AS (\n" +
-                "   SELECT\n" +
-                "     \"WishList\".\"id\" \"id\"\n" +
-                "   , \"WishList\".\"bookId\" \"bookId\"\n" +
-                "   FROM\n" +
-                "     (\n" +
-                "      SELECT\n" +
-                "        \"WishList\".\"id\" \"id\"\n" +
-                "      , \"WishList\".\"bookId\" \"bookId\"\n" +
-                "      FROM\n" +
-                "        (\n" +
-                "         SELECT *\n" +
-                "         FROM\n" +
-                "           table_wishlist\n" +
-                "      )  \"WishList\"\n" +
-                "   )  \"WishList\"\n" +
-                ") \n" +
-                ", People AS (\n" +
-                "   SELECT\n" +
-                "     \"People\".\"id\" \"id\"\n" +
-                "   , \"People\".\"email\" \"email\"\n" +
-                "   , \"gift\".\"gift\" \"gift\"\n" +
-                "   FROM\n" +
-                "     ((\n" +
-                "      SELECT\n" +
-                "        \"People\".\"id\" \"id\"\n" +
-                "      , \"People\".\"email\" \"email\"\n" +
-                "      FROM\n" +
-                "        (\n" +
-                "         SELECT *\n" +
-                "         FROM\n" +
-                "           table_people\n" +
-                "      )  \"People\"\n" +
-                "   )  \"People\"\n" +
-                "   LEFT JOIN (\n" +
-                "      SELECT\n" +
-                "        \"People\".\"id\"\n" +
-                "      , \"WishList\".\"bookId\" \"gift\"\n" +
-                "      FROM\n" +
-                "        ((\n" +
-                "         SELECT\n" +
-                "           id \"id\"\n" +
-                "         , id \"id\"\n" +
-                "         FROM\n" +
-                "           (\n" +
-                "            SELECT *\n" +
-                "            FROM\n" +
-                "              table_people\n" +
-                "         )  \"People\"\n" +
-                "      )  \"People\"\n" +
-                "      LEFT JOIN \"WishList\" ON (WishList.id = People.id))\n" +
-                "   )  \"gift\" ON (\"People\".\"id\" = \"gift\".\"id\"))\n" +
-                ")\n";
-
-        @Language("SQL") String withBookQuery = withPeopleQuery +
-                ", Book AS (\n" +
-                "   SELECT\n" +
-                "     \"Book\".\"bookId\" \"bookId\"\n" +
-                "   , \"Book\".\"authorId\" \"authorId\"\n" +
-                "   , \"Book\".\"publish_date\" \"publish_date\"\n" +
-                "   , \"Book\".\"publish_year\" \"publish_year\"\n" +
-                "   , \"author_gift_id\".\"author_gift_id\" \"author_gift_id\"\n" +
-                "   FROM\n" +
-                "     ((\n" +
-                "      SELECT\n" +
-                "        \"Book\".\"bookId\" \"bookId\"\n" +
-                "      , \"Book\".\"authorId\" \"authorId\"\n" +
-                "      , \"Book\".\"publish_date\" \"publish_date\"\n" +
-                "      , date_trunc('year', publish_date) \"publish_year\"\n" +
-                "      FROM\n" +
-                "        (\n" +
-                "         SELECT *\n" +
-                "         FROM\n" +
-                "           table_book\n" +
-                "      )  \"Book\"\n" +
-                "   )  \"Book\"\n" +
-                "   LEFT JOIN (\n" +
-                "      SELECT\n" +
-                "        \"Book\".\"bookId\"\n" +
-                "      , \"WishList\".\"bookId\" \"author_gift_id\"\n" +
-                "      FROM\n" +
-                "        (((\n" +
-                "         SELECT\n" +
-                "           bookId \"bookId\"\n" +
-                "         , authorId \"authorId\"\n" +
-                "         FROM\n" +
-                "           (\n" +
-                "            SELECT *\n" +
-                "            FROM\n" +
-                "              table_book\n" +
-                "         )  \"Book\"\n" +
-                "      )  \"Book\"\n" +
-                "      LEFT JOIN \"People\" ON (People.id = Book.authorId))\n" +
-                "      LEFT JOIN \"WishList\" ON (WishList.id = People.id))\n" +
-                "   )  \"author_gift_id\" ON (\"Book\".\"bookId\" = \"author_gift_id\".\"bookId\"))\n" +
-                ")\n";
-
-        assertSqlEqualsAndValid(rewrite("SELECT * FROM People"), "WITH " + withPeopleQuery + "SELECT * FROM People");
-        assertSqlEqualsAndValid(rewrite("SELECT * FROM People WHERE id = 'SN1001'"), "WITH " + withPeopleQuery + "SELECT * FROM People WHERE id = 'SN1001'");
-        assertSqlEqualsAndValid(rewrite("SELECT * FROM Book"), "WITH " + withBookQuery + "SELECT * FROM Book");
+        assertSqlEqualsAndValid(rewrite("SELECT * FROM People"), "WITH " + WITH_PEOPLE_QUERY + "SELECT * FROM People");
+        assertSqlEqualsAndValid(rewrite("SELECT * FROM People WHERE id = 'SN1001'"), "WITH " + WITH_PEOPLE_QUERY + "SELECT * FROM People WHERE id = 'SN1001'");
+        assertSqlEqualsAndValid(rewrite("SELECT * FROM Book"), "WITH " + WITH_BOOK_QUERY + "SELECT * FROM Book");
         assertSqlEqualsAndValid(rewrite("SELECT * FROM People a join Book b ON a.id = b.authorId WHERE a.id = 'SN1001'"),
-                "WITH " + withBookQuery + "SELECT * FROM People a join Book b ON a.id = b.authorId WHERE a.id = 'SN1001'");
+                "WITH " + WITH_BOOK_QUERY + "SELECT * FROM People a join Book b ON a.id = b.authorId WHERE a.id = 'SN1001'");
         assertSqlEqualsAndValid(rewrite("SELECT * FROM People a join WishList b ON a.id = b.id WHERE a.id = 'SN1001'"),
-                "WITH " + withPeopleQuery + "SELECT * FROM People a join WishList b ON a.id = b.id WHERE a.id = 'SN1001'");
+                "WITH " + WITH_PEOPLE_QUERY + "SELECT * FROM People a join WishList b ON a.id = b.id WHERE a.id = 'SN1001'");
 
         assertSqlEqualsAndValid(rewrite("WITH a AS (SELECT * FROM WishList) SELECT * FROM a JOIN People ON a.id = People.id"),
-                "WITH" + withPeopleQuery + ", a AS (SELECT * FROM WishList) SELECT * FROM a JOIN People ON a.id = People.id");
+                "WITH" + WITH_PEOPLE_QUERY + ", a AS (SELECT * FROM WishList) SELECT * FROM a JOIN People ON a.id = People.id");
         // rewrite table in with query
         assertSqlEqualsAndValid(rewrite("WITH a AS (SELECT * FROM People) SELECT * FROM a"),
-                "WITH" + withPeopleQuery + ", a AS (SELECT * FROM People) SELECT * FROM a");
+                "WITH" + WITH_PEOPLE_QUERY + ", a AS (SELECT * FROM People) SELECT * FROM a");
     }
 
     @Test
@@ -293,14 +296,24 @@ public class TestModelSqlRewrite
                 .hasMessage("found relation in relation join condition in WishList.peopleId");
     }
 
-    private String rewrite(String sql)
+    private static String rewrite(String sql)
     {
         return rewrite(sql, ACCIOMDL);
     }
 
-    private String rewrite(String sql, AccioMDL mdl)
+    private static String rewrite(String sql, AccioMDL mdl)
     {
         return AccioPlanner.rewrite(sql, DEFAULT_SESSION_CONTEXT, mdl, List.of(ACCIO_SQL_REWRITE));
+    }
+
+    private static void assertSqlEquals(String actual, String expected)
+    {
+        SqlParser sqlParser = new SqlParser();
+        ParsingOptions parsingOptions = new ParsingOptions(AS_DECIMAL);
+        Statement actualStmt = sqlParser.createStatement(actual, parsingOptions);
+        Statement expectedStmt = sqlParser.createStatement(expected, parsingOptions);
+        assertThat(formatSql(actualStmt))
+                .isEqualTo(formatSql(expectedStmt));
     }
 
     private void assertSqlEqualsAndValid(@Language("SQL") String actual, @Language("SQL") String expected)
@@ -309,15 +322,5 @@ public class TestModelSqlRewrite
         assertThatNoException()
                 .describedAs(format("actual sql: %s is invalid", actual))
                 .isThrownBy(() -> query(actual));
-    }
-
-    private void assertSqlEquals(String actual, String expected)
-    {
-        SqlParser sqlParser = new SqlParser();
-        ParsingOptions parsingOptions = new ParsingOptions(AS_DECIMAL);
-        Statement actualStmt = sqlParser.createStatement(actual, parsingOptions);
-        Statement expectedStmt = sqlParser.createStatement(expected, parsingOptions);
-        assertThat(formatSql(actualStmt))
-                .isEqualTo(formatSql(expectedStmt));
     }
 }
