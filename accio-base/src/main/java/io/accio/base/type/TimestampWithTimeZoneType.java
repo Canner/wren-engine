acb1d2d89f2b766d00c410e74a2e9f4da431aba9
Jia-Xuan Liu <liugs963@gmail.com>
2023-12-22 12:45:54 +0800

    support pgtype timestamp with time zone type

--- accio-base/src/main/java/io/accio/base/type/TimestampWithTimeZoneType.java
+++ accio-base/src/main/java/io/accio/base/type/TimestampWithTimeZoneType.java
@@ -14,14 +14,43 @@
 
 package io.accio.base.type;
 
+import com.google.common.annotations.VisibleForTesting;
 import io.netty.buffer.ByteBuf;
+import org.joda.time.format.DateTimeFormat;
 
 import javax.annotation.Nonnull;
+
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.ResolverStyle;
+import java.time.temporal.TemporalAccessor;
+import java.util.Locale;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_TIME;
+import static java.time.temporal.ChronoField.MILLI_OF_SECOND;
 
 public class TimestampWithTimeZoneType
         extends BaseTimestampType
 {
     public static final PGType TIMESTAMP_WITH_TIMEZONE = new TimestampWithTimeZoneType();
+    static final DateTimeFormatter PG_TIMESTAMP = new DateTimeFormatterBuilder()
+            .parseCaseInsensitive()
+            .append(ISO_LOCAL_DATE)
+            .optionalStart()
+            .appendLiteral(' ')
+            .append(ISO_LOCAL_TIME)
+            .optionalStart()
+            .appendLiteral(' ')
+            .optionalEnd()
+            .appendPattern("[VV][x][xx][xxx][z]")
+            .toFormatter(Locale.ENGLISH).withResolverStyle(ResolverStyle.STRICT);
+
+    private static final org.joda.time.format.DateTimeFormatter ISO_FORMATTER =
+            DateTimeFormat.forPattern("YYYY-MM-dd HH:mm:ss.SSS+00").withZoneUTC().withLocale(Locale.ENGLISH);
 
     private static final int OID = 1184;
     private static final String NAME = "timestamptz";
@@ -40,18 +69,30 @@ public class TimestampWithTimeZoneType
     @Override
     public byte[] encodeAsUTF8Text(@Nonnull Object value)
     {
-        throw new UnsupportedOperationException();
+        // TODO: consider AD, BC and dynamic fraction precision.
+        if (value instanceof String) {
+            return ((String) value).getBytes(UTF_8);
+        }
+        return ISO_FORMATTER.print(Instant.from((TemporalAccessor) value).getLong(MILLI_OF_SECOND))
+                .getBytes(UTF_8);
     }
 
     @Override
     public Object decodeUTF8Text(byte[] bytes)
     {
-        throw new UnsupportedOperationException();
+        String dtString = new String(bytes, UTF_8);
+        return PG_TIMESTAMP.format(tryParse(dtString));
+    }
+
+    @VisibleForTesting
+    ZonedDateTime tryParse(String timeString)
+    {
+        return ZonedDateTime.parse(timeString, PG_TIMESTAMP);
     }
 
     @Override
     public int writeAsBinary(ByteBuf buffer, @Nonnull Object value)
     {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 }
