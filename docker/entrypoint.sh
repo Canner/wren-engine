52a033495889c1ad457d3f97ded6249c20f25992
Grieve <grieve54706@gmail.com>
2024-04-18 16:03:39 +0800

    Add comment

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,6 @@
 python3 wren-sqlglot-server/main.py &
 
+# Required add-opens=java.nio=ALL-UNNAMED for Apache arrow in the Snowflake
 java -Dconfig=etc/config.properties \
      --add-opens=java.base/java.nio=ALL-UNNAMED \
      -jar $1
