bb209422fbaf15295361fb6b2c1fad5d686b637e
Jia-Xuan Liu <liugs963@gmail.com>
2024-05-13 15:19:49 +0800

    add supported function for each data source

---
+++ wren-tests/src/test/java/io/wren/testing/duckdb/TestFunctionDuckDB.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.wren.testing.duckdb;
+
+import com.google.common.collect.ImmutableMap;
+import io.wren.base.dto.Manifest;
+import io.wren.testing.AbstractFunctionTest;
+import io.wren.testing.TestingWrenServer;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static io.wren.base.config.WrenConfig.DataSourceType.DUCKDB;
+import static io.wren.base.config.WrenConfig.WREN_DATASOURCE_TYPE;
+import static io.wren.base.config.WrenConfig.WREN_DIRECTORY;
+import static io.wren.base.dto.Manifest.MANIFEST_JSON_CODEC;
+
+public class TestFunctionDuckDB
+        extends AbstractFunctionTest
+{
+    @Override
+    protected TestingWrenServer createWrenServer()
+            throws Exception
+    {
+        Path mdlDir;
+
+        try {
+            mdlDir = Files.createTempDirectory("wrenmdls");
+            Path wrenMDLFilePath = mdlDir.resolve("wrenmdl.json");
+            Files.write(wrenMDLFilePath, MANIFEST_JSON_CODEC.toJsonBytes(Manifest.builder().setCatalog("wren").setSchema("test").build()));
+        }
+        catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        ImmutableMap.Builder<String, String> properties = ImmutableMap.<String, String>builder()
+                .put(WREN_DIRECTORY, mdlDir.toAbsolutePath().toString())
+                .put(WREN_DATASOURCE_TYPE, DUCKDB.name());
+
+        return TestingWrenServer.builder()
+                .setRequiredConfigs(properties.build())
+                .build();
+    }
+}
