874d2ae9c459ca9f5ba1b99838c98c3c2f04c30d
Grieve <grieve54706@gmail.com>
2024-05-23 11:32:26 +0800

    Add validation for WREN_ENGINE_ENDPOINT

--- ibis-server/app/config.py
+++ ibis-server/app/config.py
@@ -7,6 +7,12 @@ class Config:
     def __init__(self):
         load_dotenv(override=True)
         self.wren_engine_endpoint = os.getenv('WREN_ENGINE_ENDPOINT')
+        self.validate_wren_engine_endpoint(self.wren_engine_endpoint)
+
+    @staticmethod
+    def validate_wren_engine_endpoint(endpoint):
+        if endpoint is None:
+            raise ValueError('WREN_ENGINE_ENDPOINT is not set')
 
 
 config = Config()
