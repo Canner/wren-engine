a8b11f43e4b4854d20b7cfcb7be02bdc60af4b0f
Jia-Xuan Liu <liugs963@gmail.com>
2024-05-23 09:00:01 +0800

    add some cache for column analyze

--- wren-modeling-rs/src/mdl/mod.rs
+++ wren-modeling-rs/src/mdl/mod.rs
@@ -1,20 +1,50 @@
 pub mod manifest;
 
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
 use datafusion::{config::ConfigOptions, error::DataFusionError, optimizer::analyzer::Analyzer, sql::{planner::SqlToRel, sqlparser::{dialect::GenericDialect, parser::Parser}, unparser::plan_to_sql}};
 
-use crate::{logical_plan::{rule::{ModelAnalyzeRule, ModelGenerationRule}, wren_context_provider::WrenContextProvider}, mdl::manifest::{Manifest, Model}};
-
+use crate::{logical_plan::{rule::{ModelAnalyzeRule, ModelGenerationRule}, wren_context_provider::WrenContextProvider}, mdl::manifest::{Manifest, Model, Column, Metric}};
 
 // This is the main struct that holds the manifest and provides methods to access the models
 pub struct WrenMDL {
     pub manifest: Manifest,
+    pub qualifed_references: HashMap<String, ColumnReference>,
 }
 
 impl WrenMDL {
     pub fn new(manifest: Manifest) -> Self {
-        WrenMDL { manifest }
+        let mut qualifed_references = HashMap::new();
+        manifest.models.iter().for_each(|model| {
+            model.columns.iter().for_each(|column| {
+                qualifed_references.insert(
+                    format!("{}.{}", model.name, column.name),
+                    ColumnReference::new(Dataset::Model(Arc::clone(model)), Arc::clone(column)),
+                );
+            });
+        });
+        manifest.metrics.iter().for_each(|metric| {
+            metric.dimension.iter().for_each(|dimension| {
+                qualifed_references.insert(
+                    format!("{}.{}", metric.name, dimension.name),
+                    ColumnReference::new(
+                        Dataset::Metric(Arc::clone(metric)),
+                        Arc::clone(dimension),
+                    ),
+                );
+            });
+            metric.measure.iter().for_each(|measure| {
+                qualifed_references.insert(
+                    format!("{}.{}", metric.name, measure.name),
+                    ColumnReference::new(Dataset::Metric(Arc::clone(metric)), Arc::clone(measure)),
+                );
+            });
+        });
+
+        WrenMDL {
+            manifest,
+            qualifed_references,
+        }
     }
 
     pub fn new_ref(manifest: Manifest) -> Arc<Self> {
@@ -76,39 +106,63 @@ pub fn transform_sql(wren_mdl: Arc<WrenMDL>, sql: &str) -> Result<String, DataFu
     }
 }
 
+/// Analyze the decision point. It's same as the /v1/analysis/sql API in wren engine
+pub fn decision_point_analyze(wren_mdl: Arc<WrenMDL>, sql: &str) {
+
+}
+
+pub struct ColumnReference {
+    dataset: Dataset,
+    column: Arc<Column>,
+}
+
+impl ColumnReference {
+    fn new(dataset: Dataset, column: Arc<Column>) -> Self {
+        ColumnReference { dataset, column }
+    }
+
+    pub fn get_column(&self) -> Arc<Column> {
+        Arc::clone(&self.column)
+    }
+}
+
+enum Dataset {
+    Model(Arc<Model>),
+    Metric(Arc<Metric>),
+}
+
 #[cfg(test)]
 mod test {
     use std::fs;
     use std::path::PathBuf;
     use std::sync::Arc;
+    use std::error::Error;
     use crate::mdl::manifest::Manifest;
     use crate::mdl::{self, WrenMDL};
 
     #[test]
-    fn test_projection_scan() {
+    fn test_access_model() -> Result<(), Box<dyn Error>> {
         let test_data: PathBuf = [env!("CARGO_MANIFEST_DIR"), "tests", "data", "mdl.json"]
         .iter()
         .collect();
-        let mdl_json = fs::read_to_string(test_data.as_path()).unwrap();
-        let mdl = serde_json::from_str::<Manifest>(&mdl_json).unwrap();
+        let mdl_json = fs::read_to_string(test_data.as_path())?;
+        let mdl = serde_json::from_str::<Manifest>(&mdl_json)?;
         let wren_mdl = Arc::new(WrenMDL::new(mdl));
 
-        let sql: &str = "select orderkey from orders";
-        let planned = mdl::transform_sql(Arc::clone(&wren_mdl), sql).unwrap();
-        assert_eq!(planned, r#"SELECT "orders"."orderkey" FROM (SELECT "o_orderkey" AS "orderkey", "o_custkey" AS "custkey", "o_totalprice" AS "totalprice" FROM "orders") AS "orders""#);
-    }
+        let tests: Vec<(&str, &str)> = vec![
+            ("select orderkey + orderkey from orders",
+            r#"SELECT "orders"."orderkey" + "orders"."orderkey" FROM (SELECT "o_orderkey" AS "orderkey" FROM "orders") AS "orders""#),
+            ("select orderkey from orders where orders.totalprice > 10",
+            r#"SELECT "orders"."orderkey" FROM (SELECT "o_orderkey" AS "orderkey", "o_totalprice" AS "totalprice" FROM "orders") AS "orders" WHERE ("orders"."totalprice" > 10)"#),
+            ("select orderkey, count(*) from orders where orders.totalprice > 10 group by 1",
+            r#"SELECT "orders"."orderkey", COUNT(*) FROM (SELECT "o_orderkey" AS "orderkey", "o_totalprice" AS "totalprice" FROM "orders") AS "orders" WHERE ("orders"."totalprice" > 10) GROUP BY "orders"."orderkey""#),
+        ];
 
-    #[test]
-    fn test_projection_filter_scan() {
-        let test_data: PathBuf = [env!("CARGO_MANIFEST_DIR"), "tests", "data", "mdl.json"]
-        .iter()
-        .collect();
-        let mdl_json = fs::read_to_string(test_data.as_path()).unwrap();
-        let mdl = serde_json::from_str::<Manifest>(&mdl_json).unwrap();
-        let wren_mdl = Arc::new(WrenMDL::new(mdl));
+        for (sql, expected) in tests {
+            let actual = mdl::transform_sql(Arc::clone(&wren_mdl), sql)?;
+            assert_eq!(actual, expected);
+        }
 
-        let sql: &str = "select orderkey from orders where orders.totalprice > 10";
-        let planned = mdl::transform_sql(Arc::clone(&wren_mdl), sql).unwrap();
-        assert_eq!(planned, r#"SELECT "orders"."orderkey" FROM (SELECT "o_orderkey" AS "orderkey", "o_custkey" AS "custkey", "o_totalprice" AS "totalprice" FROM "orders") AS "orders" WHERE ("orders"."totalprice" > 10)"#);
+        Ok(())
     }
 }
