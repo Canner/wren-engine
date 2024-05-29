5dd30d0b8435d437edcfa53f34c271ab0def5617
Jia-Xuan Liu <liugs963@gmail.com>
2024-05-29 15:32:46 +0800

    add heap_size setting env for engine

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+export ENV_MAX_HEAP_SIZE=$2
+export ENV_INI_HEAP_SIZE=$3
 python3 wren-sqlglot-server/main.py &
 
 # Required add-opens=java.nio=ALL-UNNAMED for Apache arrow in the Snowflake
-java -Dconfig=etc/config.properties \
+java -Xmx${ENV_MAX_HEAP_SIZE:-"512m"} -Xms${ENV_INI_HEAP_SIZE:-"64m"}  -Dconfig=etc/config.properties \
      --add-opens=java.base/java.nio=ALL-UNNAMED \
      -jar $1
