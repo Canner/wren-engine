747d2d7e6abca63cba4d5279c807d7cd98dceb2b
Grieve <grieve54706@gmail.com>
2024-05-31 16:49:08 +0800

    Deconstruct Rewriter

--- ibis-server/app/mdl/rewriter.py
+++ ibis-server/app/mdl/rewriter.py
@@ -7,19 +7,16 @@ from app.util import log_rewritten
 wren_engine_endpoint = get_config().wren_engine_endpoint
 
 
-class Rewriter:
-
-    @staticmethod
-    @log_rewritten
-    def rewrite(manifest_str: str, sql: str) -> str:
-        try:
-            r = httpx.request(
-                method='GET',
-                url=f'{wren_engine_endpoint}/v2/mdl/dry-plan',
-                headers={'Content-Type': 'application/json', 'Accept': 'application/json'},
-                content=orjson.dumps({
-                    'manifestStr': manifest_str,
-                    'sql': sql}))
-            return r.text if r.status_code == httpx.codes.OK else r.raise_for_status()
-        except httpx.ConnectError as e:
-            raise ConnectionError(f'Cannot connect to Wren Engine: {e}')
+@log_rewritten
+def rewrite(manifest_str: str, sql: str) -> str:
+    try:
+        r = httpx.request(
+            method='GET',
+            url=f'{wren_engine_endpoint}/v2/mdl/dry-plan',
+            headers={'Content-Type': 'application/json', 'Accept': 'application/json'},
+            content=orjson.dumps({
+                'manifestStr': manifest_str,
+                'sql': sql}))
+        return r.text if r.status_code == httpx.codes.OK else r.raise_for_status()
+    except httpx.ConnectError as e:
+        raise ConnectionError(f'Cannot connect to Wren Engine: {e}')
--- ibis-server/app/routers/ibis.py
+++ ibis-server/app/routers/ibis.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.mdl.rewriter import Rewriter
+from app.mdl.rewriter import rewrite
 from app.model.data_source import DataSource, ConnectionInfo
 from app.model.dto import PostgresDTO, BigQueryDTO, SnowflakeDTO
 from app.util import log_dto, to_json
@@ -11,21 +11,21 @@ router = APIRouter(prefix="/v2/ibis")
 @router.post("/postgres/query")
 @log_dto
 def query_postgres(dto: PostgresDTO) -> dict:
-    rewritten_sql = Rewriter.rewrite(dto.manifest_str, dto.sql)
+    rewritten_sql = rewrite(dto.manifest_str, dto.sql)
     return query_data(DataSource.postgres, dto.connection_info, rewritten_sql)
 
 
 @router.post("/bigquery/query")
 @log_dto
 def query_bigquery(dto: BigQueryDTO) -> dict:
-    rewritten_sql = Rewriter.rewrite(dto.manifest_str, dto.sql)
+    rewritten_sql = rewrite(dto.manifest_str, dto.sql)
     return query_data(DataSource.bigquery, dto.connection_info, rewritten_sql)
 
 
 @router.post("/snowflake/query")
 @log_dto
 def query_snowflake(dto: SnowflakeDTO) -> dict:
-    rewritten_sql = Rewriter.rewrite(dto.manifest_str, dto.sql)
+    rewritten_sql = rewrite(dto.manifest_str, dto.sql)
     return query_data(DataSource.snowflake, dto.connection_info, rewritten_sql)
 
 
