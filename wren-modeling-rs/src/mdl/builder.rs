e11d4155458883d125d181abe6cd574c21027bd5
Jia-Xuan Liu <liugs963@gmail.com>
2024-06-06 17:57:02 +0800

    remove with_ prefix for builder method

--- wren-modeling-rs/src/mdl/builder.rs
+++ wren-modeling-rs/src/mdl/builder.rs
@@ -24,32 +24,32 @@ impl ManifestBuilder {
         }
     }
 
-    pub fn with_catalog(mut self, catalog: &str) -> Self {
+    pub fn catalog(mut self, catalog: &str) -> Self {
         self.manifest.catalog = catalog.to_string();
         self
     }
 
-    pub fn with_schema(mut self, schema: &str) -> Self {
+    pub fn schema(mut self, schema: &str) -> Self {
         self.manifest.schema = schema.to_string();
         self
     }
 
-    pub fn with_model(mut self, model: Arc<Model>) -> Self {
+    pub fn model(mut self, model: Arc<Model>) -> Self {
         self.manifest.models.push(model);
         self
     }
 
-    pub fn with_relationship(mut self, relationship: Arc<Relationship>) -> Self {
+    pub fn relationship(mut self, relationship: Arc<Relationship>) -> Self {
         self.manifest.relationships.push(relationship);
         self
     }
 
-    pub fn with_metric(mut self, metric: Arc<Metric>) -> Self {
+    pub fn metric(mut self, metric: Arc<Metric>) -> Self {
         self.manifest.metrics.push(metric);
         self
     }
 
-    pub fn with_view(mut self, view: Arc<View>) -> Self {
+    pub fn view(mut self, view: Arc<View>) -> Self {
         self.manifest.views.push(view);
         self
     }
@@ -80,42 +80,42 @@ impl ModelBuilder {
         }
     }
 
-    pub fn with_ref_sql(mut self, ref_sql: &str) -> Self {
+    pub fn ref_sql(mut self, ref_sql: &str) -> Self {
         self.model.ref_sql = ref_sql.to_string();
         self
     }
 
-    pub fn with_base_object(mut self, base_object: &str) -> Self {
+    pub fn base_object(mut self, base_object: &str) -> Self {
         self.model.base_object = base_object.to_string();
         self
     }
 
-    pub fn with_table_reference(mut self, table_reference: &str) -> Self {
+    pub fn table_reference(mut self, table_reference: &str) -> Self {
         self.model.table_reference = table_reference.to_string();
         self
     }
 
-    pub fn with_column(mut self, column: Arc<Column>) -> Self {
+    pub fn column(mut self, column: Arc<Column>) -> Self {
         self.model.columns.push(column);
         self
     }
 
-    pub fn with_primary_key(mut self, primary_key: &str) -> Self {
+    pub fn primary_key(mut self, primary_key: &str) -> Self {
         self.model.primary_key = primary_key.to_string();
         self
     }
 
-    pub fn with_cached(mut self, cached: bool) -> Self {
+    pub fn cached(mut self, cached: bool) -> Self {
         self.model.cached = cached;
         self
     }
 
-    pub fn with_refresh_time(mut self, refresh_time: &str) -> Self {
+    pub fn refresh_time(mut self, refresh_time: &str) -> Self {
         self.model.refresh_time = refresh_time.to_string();
         self
     }
 
-    pub fn with_property(mut self, key: &str, value: &str) -> Self {
+    pub fn property(mut self, key: &str, value: &str) -> Self {
         self.model
             .properties
             .push((key.to_string(), value.to_string()));
@@ -146,27 +146,27 @@ impl ColumnBuilder {
         }
     }
 
-    pub fn with_relationship(mut self, relationship: &str) -> Self {
+    pub fn relationship(mut self, relationship: &str) -> Self {
         self.column.relationship = Some(relationship.to_string());
         self
     }
 
-    pub fn with_is_calculated(mut self, is_calculated: bool) -> Self {
+    pub fn is_calculated(mut self, is_calculated: bool) -> Self {
         self.column.is_calculated = is_calculated;
         self
     }
 
-    pub fn with_no_null(mut self, no_null: bool) -> Self {
+    pub fn no_null(mut self, no_null: bool) -> Self {
         self.column.no_null = no_null;
         self
     }
 
-    pub fn with_expression(mut self, expression: &str) -> Self {
+    pub fn expression(mut self, expression: &str) -> Self {
         self.column.expression = Some(expression.to_string());
         self
     }
 
-    pub fn with_property(mut self, key: &str, value: &str) -> Self {
+    pub fn property(mut self, key: &str, value: &str) -> Self {
         self.column
             .properties
             .push((key.to_string(), value.to_string()));
@@ -195,22 +195,22 @@ impl RelationshipBuilder {
         }
     }
 
-    pub fn with_model(mut self, model: &str) -> Self {
+    pub fn model(mut self, model: &str) -> Self {
         self.relationship.models.push(model.to_string());
         self
     }
 
-    pub fn with_join_type(mut self, join_type: JoinType) -> Self {
+    pub fn join_type(mut self, join_type: JoinType) -> Self {
         self.relationship.join_type = join_type;
         self
     }
 
-    pub fn with_condition(mut self, condition: &str) -> Self {
+    pub fn condition(mut self, condition: &str) -> Self {
         self.relationship.condition = condition.to_string();
         self
     }
 
-    pub fn with_property(mut self, key: &str, value: &str) -> Self {
+    pub fn property(mut self, key: &str, value: &str) -> Self {
         self.relationship
             .properties
             .push((key.to_string(), value.to_string()));
@@ -242,32 +242,32 @@ impl MetricBuilder {
         }
     }
 
-    pub fn with_dimension(mut self, dimension: Arc<Column>) -> Self {
+    pub fn dimension(mut self, dimension: Arc<Column>) -> Self {
         self.metric.dimension.push(dimension);
         self
     }
 
-    pub fn with_measure(mut self, measure: Arc<Column>) -> Self {
+    pub fn measure(mut self, measure: Arc<Column>) -> Self {
         self.metric.measure.push(measure);
         self
     }
 
-    pub fn with_time_grain(mut self, time_grain: TimeGrain) -> Self {
+    pub fn time_grain(mut self, time_grain: TimeGrain) -> Self {
         self.metric.time_grain.push(time_grain);
         self
     }
 
-    pub fn with_cached(mut self, cached: bool) -> Self {
+    pub fn cached(mut self, cached: bool) -> Self {
         self.metric.cached = cached;
         self
     }
 
-    pub fn with_refresh_time(mut self, refresh_time: &str) -> Self {
+    pub fn refresh_time(mut self, refresh_time: &str) -> Self {
         self.metric.refresh_time = refresh_time.to_string();
         self
     }
 
-    pub fn with_property(mut self, key: &str, value: &str) -> Self {
+    pub fn property(mut self, key: &str, value: &str) -> Self {
         self.metric
             .properties
             .push((key.to_string(), value.to_string()));
@@ -294,12 +294,12 @@ impl TimeGrainBuilder {
         }
     }
 
-    pub fn with_ref_column(mut self, ref_column: &str) -> Self {
+    pub fn ref_column(mut self, ref_column: &str) -> Self {
         self.time_grain.ref_column = ref_column.to_string();
         self
     }
 
-    pub fn with_date_part(mut self, date_part: TimeUnit) -> Self {
+    pub fn date_part(mut self, date_part: TimeUnit) -> Self {
         self.time_grain.date_parts.push(date_part);
         self
     }
@@ -324,12 +324,12 @@ impl ViewBuilder {
         }
     }
 
-    pub fn with_statement(mut self, statement: &str) -> Self {
+    pub fn statement(mut self, statement: &str) -> Self {
         self.view.statement = statement.to_string();
         self
     }
 
-    pub fn with_property(mut self, key: &str, value: &str) -> Self {
+    pub fn property(mut self, key: &str, value: &str) -> Self {
         self.view
             .properties
             .push((key.to_string(), value.to_string()));
@@ -353,11 +353,11 @@ mod test {
     #[test]
     fn test_column_roundtrip() {
         let expected = ColumnBuilder::new("id", "integer")
-            .with_relationship("test")
-            .with_is_calculated(true)
-            .with_no_null(true)
-            .with_expression("test")
-            .with_property("key", "value")
+            .relationship("test")
+            .is_calculated(true)
+            .no_null(true)
+            .expression("test")
+            .property("key", "value")
             .build();
 
         let json_str = serde_json::to_string(&expected).unwrap();
@@ -368,14 +368,14 @@ mod test {
     #[test]
     fn test_model_roundtrip() {
         let model = ModelBuilder::new("test")
-            .with_ref_sql("SELECT * FROM test")
-            .with_base_object("test")
-            .with_table_reference("test")
-            .with_column(ColumnBuilder::new("id", "integer").build())
-            .with_primary_key("id")
-            .with_cached(true)
-            .with_refresh_time("1h")
-            .with_property("key", "value")
+            .ref_sql("SELECT * FROM test")
+            .base_object("test")
+            .table_reference("test")
+            .column(ColumnBuilder::new("id", "integer").build())
+            .primary_key("id")
+            .cached(true)
+            .refresh_time("1h")
+            .property("key", "value")
             .build();
 
         let json_str = serde_json::to_string(&model).unwrap();
@@ -386,11 +386,11 @@ mod test {
     #[test]
     fn test_relationship_roundtrip() {
         let expected = RelationshipBuilder::new("test")
-            .with_model("testA")
-            .with_model("testB")
-            .with_join_type(JoinType::OneToMany)
-            .with_condition("test")
-            .with_property("key", "value")
+            .model("testA")
+            .model("testB")
+            .join_type(JoinType::OneToMany)
+            .condition("test")
+            .property("key", "value")
             .build();
 
         let json_str = serde_json::to_string(&expected).unwrap();
@@ -401,17 +401,17 @@ mod test {
     #[test]
     fn test_metric_roundtrip() {
         let model = MetricBuilder::new("test")
-            .with_dimension(ColumnBuilder::new("dim", "integer").build())
-            .with_measure(ColumnBuilder::new("mea", "integer").build())
-            .with_time_grain(
+            .dimension(ColumnBuilder::new("dim", "integer").build())
+            .measure(ColumnBuilder::new("mea", "integer").build())
+            .time_grain(
                 TimeGrainBuilder::new("tg")
-                    .with_ref_column("tg")
-                    .with_date_part(TimeUnit::Day)
+                    .ref_column("tg")
+                    .date_part(TimeUnit::Day)
                     .build(),
             )
-            .with_cached(true)
-            .with_refresh_time("1h")
-            .with_property("key", "value")
+            .cached(true)
+            .refresh_time("1h")
+            .property("key", "value")
             .build();
 
         let json_str = serde_json::to_string(&model).unwrap();
@@ -422,8 +422,8 @@ mod test {
     #[test]
     fn test_view_roundtrip() {
         let expected = ViewBuilder::new("test")
-            .with_statement("SELECT * FROM test")
-            .with_property("key", "value")
+            .statement("SELECT * FROM test")
+            .property("key", "value")
             .build();
 
         let json_str = serde_json::to_string(&expected).unwrap();
@@ -434,50 +434,50 @@ mod test {
     #[test]
     fn test_manifest_roundtrip() {
         let model = ModelBuilder::new("test")
-            .with_ref_sql("SELECT * FROM test")
-            .with_base_object("test")
-            .with_table_reference("test")
-            .with_column(ColumnBuilder::new("id", "integer").build())
-            .with_primary_key("id")
-            .with_cached(true)
-            .with_refresh_time("1h")
-            .with_property("key", "value")
+            .ref_sql("SELECT * FROM test")
+            .base_object("test")
+            .table_reference("test")
+            .column(ColumnBuilder::new("id", "integer").build())
+            .primary_key("id")
+            .cached(true)
+            .refresh_time("1h")
+            .property("key", "value")
             .build();
 
         let relationship = RelationshipBuilder::new("test")
-            .with_model("testA")
-            .with_model("testB")
-            .with_join_type(JoinType::OneToMany)
-            .with_condition("test")
-            .with_property("key", "value")
+            .model("testA")
+            .model("testB")
+            .join_type(JoinType::OneToMany)
+            .condition("test")
+            .property("key", "value")
             .build();
 
         let metric = MetricBuilder::new("test")
-            .with_dimension(ColumnBuilder::new("dim", "integer").build())
-            .with_measure(ColumnBuilder::new("mea", "integer").build())
-            .with_time_grain(
+            .dimension(ColumnBuilder::new("dim", "integer").build())
+            .measure(ColumnBuilder::new("mea", "integer").build())
+            .time_grain(
                 TimeGrainBuilder::new("tg")
-                    .with_ref_column("tg")
-                    .with_date_part(TimeUnit::Day)
+                    .ref_column("tg")
+                    .date_part(TimeUnit::Day)
                     .build(),
             )
-            .with_cached(true)
-            .with_refresh_time("1h")
-            .with_property("key", "value")
+            .cached(true)
+            .refresh_time("1h")
+            .property("key", "value")
             .build();
 
         let view = ViewBuilder::new("test")
-            .with_statement("SELECT * FROM test")
-            .with_property("key", "value")
+            .statement("SELECT * FROM test")
+            .property("key", "value")
             .build();
 
         let expected = crate::mdl::builder::ManifestBuilder::new()
-            .with_catalog("wrenai")
-            .with_schema("default")
-            .with_model(model)
-            .with_relationship(relationship)
-            .with_metric(metric)
-            .with_view(view)
+            .catalog("wrenai")
+            .schema("default")
+            .model(model)
+            .relationship(relationship)
+            .metric(metric)
+            .view(view)
             .build();
 
         let json_str = serde_json::to_string(&expected).unwrap();
