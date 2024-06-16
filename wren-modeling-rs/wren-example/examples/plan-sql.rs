e54a60d9cb6cae7dec4233c42ddfc92395546ab3
Jia-Xuan Liu <liugs963@gmail.com>
2024-06-16 14:43:57 +0800

    modified the example

--- wren-modeling-rs/wren-example/examples/plan-sql.rs
+++ wren-modeling-rs/wren-example/examples/plan-sql.rs
@@ -10,7 +10,7 @@ async fn main() -> datafusion::common::Result<()> {
     let manifest = init_manifest();
     let analyzed_mdl = Arc::new(AnalyzedWrenMDL::analyze(manifest));
 
-    let sql = "select * from wrenai.default.customers";
+    let sql = "select * from wrenai.default.customers_model";
     println!("Original SQL: \n{}", sql);
     let sql = transform_sql(analyzed_mdl, sql).unwrap();
     println!("Wren engine generated SQL: \n{}", sql);
@@ -20,7 +20,7 @@ async fn main() -> datafusion::common::Result<()> {
 fn init_manifest() -> Manifest {
     ManifestBuilder::new()
         .model(
-            ModelBuilder::new("customers")
+            ModelBuilder::new("customers_model")
                 .table_reference("datafusion.public.customers")
                 .column(ColumnBuilder::new("city", "varchar").build())
                 .column(ColumnBuilder::new("id", "varchar").build())
@@ -29,7 +29,7 @@ fn init_manifest() -> Manifest {
                 .build(),
         )
         .model(
-            ModelBuilder::new("order_items")
+            ModelBuilder::new("order_items_model")
                 .table_reference("datafusion.public.order_items")
                 .column(ColumnBuilder::new("freight_value", "double").build())
                 .column(ColumnBuilder::new("id", "bigint").build())
@@ -39,21 +39,21 @@ fn init_manifest() -> Manifest {
                 .column(ColumnBuilder::new("product_id", "varchar").build())
                 .column(ColumnBuilder::new("shipping_limit_date", "varchar").build())
                 .column(
-                    ColumnBuilder::new("orders", "orders")
+                    ColumnBuilder::new("orders_model", "orders_model")
                         .relationship("orders_order_items")
                         .build(),
                 )
                 .column(
                     ColumnBuilder::new("customer_state", "varchar")
                         .calculated(true)
-                        .expression("orders.customers.state")
+                        .expression("orders_model.customers_model.state")
                         .build(),
                 )
                 .primary_key("id")
                 .build(),
         )
         .model(
-            ModelBuilder::new("orders")
+            ModelBuilder::new("orders_model")
                 .table_reference("datafusion.public.orders")
                 .column(ColumnBuilder::new("approved_timestamp", "varchar").build())
                 .column(ColumnBuilder::new("customer_id", "varchar").build())
@@ -62,32 +62,32 @@ fn init_manifest() -> Manifest {
                 .column(ColumnBuilder::new("order_id", "varchar").build())
                 .column(ColumnBuilder::new("purchase_timestamp", "varchar").build())
                 .column(
-                    ColumnBuilder::new("customers", "customers")
+                    ColumnBuilder::new("customers_model", "customers_model")
                         .relationship("orders_customer")
                         .build(),
                 )
                 .column(
                     ColumnBuilder::new("customer_state", "varchar")
                         .calculated(true)
-                        .expression("customers.state")
+                        .expression("customers_model.state")
                         .build(),
                 )
                 .build(),
         )
         .relationship(
             RelationshipBuilder::new("orders_customer")
-                .model("orders")
-                .model("customers")
+                .model("orders_model")
+                .model("customers_model")
                 .join_type(JoinType::ManyToOne)
                 .condition("orders.customer_id = customers.id")
                 .build(),
         )
         .relationship(
             RelationshipBuilder::new("orders_order_items")
-                .model("orders")
-                .model("order_items")
+                .model("orders_model")
+                .model("order_items_model")
                 .join_type(JoinType::ManyToOne)
-                .condition("orders.order_id = order_items.order_id")
+                .condition("orders_model.order_id = order_items_model.order_id")
                 .build(),
         )
         .build()
