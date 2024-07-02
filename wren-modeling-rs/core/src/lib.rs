f92a22c334b1d937854a6de47a33af62b3031a1b
Jia-Xuan Liu <liugs963@gmail.com>
2024-07-02 13:27:51 +0800

    cargo fmt

--- wren-modeling-rs/core/src/lib.rs
+++ wren-modeling-rs/core/src/lib.rs
@@ -2,5 +2,5 @@ pub mod logical_plan;
 pub mod mdl;
 
 // export the datafusion SessionContext for the use of the wren-core
-pub use datafusion::execution::context::SessionContext;
 pub use datafusion::error::DataFusionError;
+pub use datafusion::execution::context::SessionContext;
