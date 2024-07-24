fdc33cfb91ce2608f5c29a6fc2528dd041143ecd
Jia-Xuan Liu <liugs963@gmail.com>
2024-07-24 17:13:27 +0800

    add v3 query api and tests

---
+++ ibis-server/app/routers/v3/connector.py
@@ -0,0 +1,74 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Query, Response
+from fastapi.responses import JSONResponse
+
+from app.dependencies import verify_query_dto
+from app.logger import log_dto
+from app.mdl.rewriter import EmbeddedEngineRewriter
+from app.model import (
+    DryPlanDTO,
+    QueryDTO,
+    ValidateDTO,
+)
+from app.model.connector import Connector
+from app.model.data_source import DataSource
+from app.model.validator import Validator
+from app.util import to_json
+
+router = APIRouter(prefix="/connector")
+
+
+@router.post("/{data_source}/query", dependencies=[Depends(verify_query_dto)])
+@log_dto
+def query(
+    data_source: DataSource,
+    dto: QueryDTO,
+    dry_run: Annotated[bool, Query(alias="dryRun")] = False,
+    limit: int | None = None,
+) -> Response:
+    try:
+        rewritten_sql = EmbeddedEngineRewriter(dto.manifest_str, data_source).rewrite(
+            dto.sql
+        )
+        connector = Connector(data_source, dto.connection_info, dto.manifest_str)
+        if dry_run:
+            connector.dry_run(rewritten_sql)
+            return Response(status_code=204)
+        return JSONResponse(
+            to_json(connector.query(rewritten_sql, limit=limit), dto.column_dtypes)
+        )
+    except Exception as e:
+        return Response(str(e), status_code=422)
+
+
+@router.post("/dry-plan")
+@log_dto
+def dry_plan(dto: DryPlanDTO) -> str:
+    try:
+        return EmbeddedEngineRewriter(dto.manifest_str).rewrite(dto.sql)
+    except Exception as e:
+        return Response(str(e), status_code=422)
+
+
+@router.post("/{data_source}/dry-plan")
+@log_dto
+def dry_plan_for_data_source(data_source: DataSource, dto: DryPlanDTO) -> str:
+    try:
+        return EmbeddedEngineRewriter(dto.manifest_str, data_source).rewrite(dto.sql)
+    except Exception as e:
+        return Response(str(e), status_code=422)
+
+
+@router.post("/{data_source}/validate/{rule_name}")
+@log_dto
+def validate(data_source: DataSource, rule_name: str, dto: ValidateDTO) -> Response:
+    try:
+        validator = Validator(
+            Connector(data_source, dto.connection_info, dto.manifest_str),
+            EmbeddedEngineRewriter(dto.manifest_str, data_source),
+        )
+        validator.validate(rule_name, dto.parameters)
+        return Response(status_code=204)
+    except Exception as e:
+        return Response(str(e), status_code=422)
---
+++ ibis-server/tests/routers/v3/connector/test_postgres.py
@@ -0,0 +1,418 @@
+import base64
+
+import orjson
+import pandas as pd
+import pytest
+import sqlalchemy
+from fastapi.testclient import TestClient
+from testcontainers.postgres import PostgresContainer
+
+from app.main import app
+from tests.confest import file_path
+
+pytestmark = pytest.mark.beta
+
+client = TestClient(app)
+
+base_url = "/v3/connector/postgres"
+
+manifest = {
+    "catalog": "wren",
+    "schema": "public",
+    "models": [
+        {
+            "name": "orders",
+            "tableReference": {
+                "schema": "public",
+                "table": "orders",
+            },
+            "columns": [
+                {"name": "orderkey", "expression": "o_orderkey", "type": "integer"},
+                {"name": "custkey", "expression": "o_custkey", "type": "integer"},
+                {
+                    "name": "orderstatus",
+                    "expression": "o_orderstatus",
+                    "type": "varchar",
+                },
+                {
+                    "name": "totalprice",
+                    "expression": "o_totalprice",
+                    "type": "double",
+                },
+                {"name": "orderdate", "expression": "o_orderdate", "type": "date"},
+                {
+                    "name": "order_cust_key",
+                    "expression": "concat(o_orderkey, '_', o_custkey)",
+                    "type": "varchar",
+                },
+                {
+                    "name": "timestamp",
+                    "expression": "cast('2024-01-01T23:59:59' as timestamp)",
+                    "type": "timestamp",
+                },
+                {
+                    "name": "timestamptz",
+                    "expression": "cast('2024-01-01T23:59:59' as timestamp with time zone)",
+                    "type": "timestamp",
+                },
+            ],
+            "primaryKey": "orderkey",
+        },
+        {
+            "name": "customer",
+            "tableReference": {
+                "schema": "public",
+                "table": "customer",
+            },
+            "columns": [
+                {"name": "custkey", "expression": "c_custkey", "type": "integer"},
+                {"name": "name", "expression": "c_name", "type": "varchar"},
+            ],
+            "primaryKey": "custkey",
+        },
+    ],
+}
+
+manifest_str = base64.b64encode(orjson.dumps(manifest)).decode("utf-8")
+
+
+@pytest.fixture(scope="module")
+def postgres(request) -> PostgresContainer:
+    pg = PostgresContainer("postgres:16-alpine").start()
+    engine = sqlalchemy.create_engine(pg.get_connection_url())
+    pd.read_parquet(file_path("resource/tpch/data/orders.parquet")).to_sql(
+        "orders", engine, index=False
+    )
+    pd.read_parquet(file_path("resource/tpch/data/customer.parquet")).to_sql(
+        "customer", engine, index=False
+    )
+    request.addfinalizer(pg.stop)
+    return pg
+
+
+def test_query(postgres: PostgresContainer):
+    connection_info = to_connection_info(postgres)
+    response = client.post(
+        url=f"{base_url}/query",
+        json={
+            "connectionInfo": connection_info,
+            "manifestStr": manifest_str,
+            "sql": "SELECT * FROM wren.public.orders LIMIT 1",
+        },
+    )
+    assert response.status_code == 200
+    result = response.json()
+    assert len(result["columns"]) == len(manifest["models"][0]["columns"])
+    assert len(result["data"]) == 1
+    assert result["data"][0] == [
+        1,
+        370,
+        "O",
+        "172799.49",
+        820540800000,
+        "1_370",
+        1704153599000,
+        1704153599000,
+    ]
+    assert result["dtypes"] == {
+        "orderkey": "int32",
+        "custkey": "int32",
+        "orderstatus": "object",
+        "totalprice": "object",
+        "orderdate": "object",
+        "order_cust_key": "object",
+        "timestamp": "datetime64[ns]",
+        "timestamptz": "datetime64[ns, UTC]",
+    }
+
+
+def test_query_with_connection_url(postgres: PostgresContainer):
+    connection_url = to_connection_url(postgres)
+    response = client.post(
+        url=f"{base_url}/query",
+        json={
+            "connectionInfo": {"connectionUrl": connection_url},
+            "manifestStr": manifest_str,
+            "sql": "SELECT * FROM wren.public.orders LIMIT 1",
+        },
+    )
+    assert response.status_code == 200
+    result = response.json()
+    assert len(result["columns"]) == len(manifest["models"][0]["columns"])
+    assert len(result["data"]) == 1
+    assert result["data"][0][0] == 1
+    assert result["dtypes"] is not None
+
+
+def test_query_with_column_dtypes(postgres: PostgresContainer):
+    connection_info = to_connection_info(postgres)
+    response = client.post(
+        url=f"{base_url}/query",
+        json={
+            "connectionInfo": connection_info,
+            "manifestStr": manifest_str,
+            "sql": "SELECT * FROM wren.public.orders LIMIT 1",
+            "columnDtypes": {
+                "totalprice": "float",
+                "orderdate": "datetime64",
+                "timestamp": "datetime64",
+                "timestamptz": "datetime64",
+            },
+        },
+    )
+    assert response.status_code == 200
+    result = response.json()
+    assert len(result["columns"]) == len(manifest["models"][0]["columns"])
+    assert len(result["data"]) == 1
+    assert result["data"][0] == [
+        1,
+        370,
+        "O",
+        172799.49,
+        "1996-01-02 00:00:00.000000",
+        "1_370",
+        "2024-01-01 23:59:59.000000",
+        "2024-01-01 23:59:59.000000 UTC",
+    ]
+    assert result["dtypes"] == {
+        "orderkey": "int32",
+        "custkey": "int32",
+        "orderstatus": "object",
+        "totalprice": "float64",
+        "orderdate": "object",
+        "order_cust_key": "object",
+        "timestamp": "object",
+        "timestamptz": "object",
+    }
+
+
+def test_query_with_limit(postgres: PostgresContainer):
+    connection_info = to_connection_info(postgres)
+    response = client.post(
+        url=f"{base_url}/query",
+        params={"limit": 1},
+        json={
+            "connectionInfo": connection_info,
+            "manifestStr": manifest_str,
+            "sql": "SELECT * FROM wren.public.orders",
+        },
+    )
+    assert response.status_code == 200
+    result = response.json()
+    assert len(result["data"]) == 1
+
+    response = client.post(
+        url=f"{base_url}/query",
+        params={"limit": 1},
+        json={
+            "connectionInfo": connection_info,
+            "manifestStr": manifest_str,
+            "sql": "SELECT * FROM wren.public.orders LIMIT 10",
+        },
+    )
+    assert response.status_code == 200
+    result = response.json()
+    assert len(result["data"]) == 1
+
+
+def test_query_without_manifest(postgres: PostgresContainer):
+    connection_info = to_connection_info(postgres)
+    response = client.post(
+        url=f"{base_url}/query",
+        json={
+            "connectionInfo": connection_info,
+            "sql": "SELECT * FROM wren.public.orders LIMIT 1",
+        },
+    )
+    assert response.status_code == 422
+    result = response.json()
+    assert result["detail"][0] is not None
+    assert result["detail"][0]["type"] == "missing"
+    assert result["detail"][0]["loc"] == ["body", "manifestStr"]
+    assert result["detail"][0]["msg"] == "Field required"
+
+
+def test_query_without_sql(postgres: PostgresContainer):
+    connection_info = to_connection_info(postgres)
+    response = client.post(
+        url=f"{base_url}/query",
+        json={"connectionInfo": connection_info, "manifestStr": manifest_str},
+    )
+    assert response.status_code == 422
+    result = response.json()
+    assert result["detail"][0] is not None
+    assert result["detail"][0]["type"] == "missing"
+    assert result["detail"][0]["loc"] == ["body", "sql"]
+    assert result["detail"][0]["msg"] == "Field required"
+
+
+def test_query_without_connection_info():
+    response = client.post(
+        url=f"{base_url}/query",
+        json={
+            "manifestStr": manifest_str,
+            "sql": "SELECT * FROM wren.public.orders LIMIT 1",
+        },
+    )
+    assert response.status_code == 422
+    result = response.json()
+    assert result["detail"][0] is not None
+    assert result["detail"][0]["type"] == "missing"
+    assert result["detail"][0]["loc"] == ["body", "connectionInfo"]
+    assert result["detail"][0]["msg"] == "Field required"
+
+
+def test_query_with_dry_run(postgres: PostgresContainer):
+    connection_info = to_connection_info(postgres)
+    response = client.post(
+        url=f"{base_url}/query",
+        params={"dryRun": True},
+        json={
+            "connectionInfo": connection_info,
+            "manifestStr": manifest_str,
+            "sql": "SELECT * FROM wren.public.orders LIMIT 1",
+        },
+    )
+    assert response.status_code == 204
+
+
+def test_query_with_dry_run_and_invalid_sql(postgres: PostgresContainer):
+    connection_info = to_connection_info(postgres)
+    response = client.post(
+        url=f"{base_url}/query",
+        params={"dryRun": True},
+        json={
+            "connectionInfo": connection_info,
+            "manifestStr": manifest_str,
+            "sql": "SELECT * FROM X",
+        },
+    )
+    assert response.status_code == 422
+    assert response.text is not None
+
+
+def test_validate_with_unknown_rule(postgres: PostgresContainer):
+    connection_info = to_connection_info(postgres)
+    response = client.post(
+        url=f"{base_url}/validate/unknown_rule",
+        json={
+            "connectionInfo": connection_info,
+            "manifestStr": manifest_str,
+            "parameters": {"modelName": "orders", "columnName": "orderkey"},
+        },
+    )
+    assert response.status_code == 422
+    assert (
+        response.text
+        == "The rule `unknown_rule` is not in the rules, rules: ['column_is_valid']"
+    )
+
+
+# TODO: enable this test after supporting the default catalog and default schema
+# def test_validate_rule_column_is_valid(postgres: PostgresContainer):
+#     connection_info = to_connection_info(postgres)
+#     response = client.post(
+#         url=f"{base_url}/validate/column_is_valid",
+#         json={
+#             "connectionInfo": connection_info,
+#             "manifestStr": manifest_str,
+#             "parameters": {"modelName": "orders", "columnName": "orderkey"},
+#         },
+#     )
+#     assert response.status_code == 204
+
+
+def test_validate_rule_column_is_valid_with_invalid_parameters(
+    postgres: PostgresContainer,
+):
+    connection_info = to_connection_info(postgres)
+    response = client.post(
+        url=f"{base_url}/validate/column_is_valid",
+        json={
+            "connectionInfo": connection_info,
+            "manifestStr": manifest_str,
+            "parameters": {"modelName": "X", "columnName": "orderkey"},
+        },
+    )
+    assert response.status_code == 422
+
+    response = client.post(
+        url=f"{base_url}/validate/column_is_valid",
+        json={
+            "connectionInfo": connection_info,
+            "manifestStr": manifest_str,
+            "parameters": {"modelName": "orders", "columnName": "X"},
+        },
+    )
+    assert response.status_code == 422
+
+
+def test_validate_rule_column_is_valid_without_parameters(postgres: PostgresContainer):
+    connection_info = to_connection_info(postgres)
+    response = client.post(
+        url=f"{base_url}/validate/column_is_valid",
+        json={"connectionInfo": connection_info, "manifestStr": manifest_str},
+    )
+    assert response.status_code == 422
+    result = response.json()
+    assert result["detail"][0] is not None
+    assert result["detail"][0]["type"] == "missing"
+    assert result["detail"][0]["loc"] == ["body", "parameters"]
+    assert result["detail"][0]["msg"] == "Field required"
+
+
+def test_validate_rule_column_is_valid_without_one_parameter(
+    postgres: PostgresContainer,
+):
+    connection_info = to_connection_info(postgres)
+    response = client.post(
+        url=f"{base_url}/validate/column_is_valid",
+        json={
+            "connectionInfo": connection_info,
+            "manifestStr": manifest_str,
+            "parameters": {"modelName": "orders"},
+        },
+    )
+    assert response.status_code == 422
+    assert response.text == "Missing required parameter: `columnName`"
+
+    response = client.post(
+        url=f"{base_url}/validate/column_is_valid",
+        json={
+            "connectionInfo": connection_info,
+            "manifestStr": manifest_str,
+            "parameters": {"columnName": "orderkey"},
+        },
+    )
+    assert response.status_code == 422
+    assert response.text == "Missing required parameter: `modelName`"
+
+
+def test_dry_plan():
+    response = client.post(
+        url=f"{base_url}/dry-plan",
+        json={
+            "manifestStr": manifest_str,
+            "sql": "SELECT orderkey, order_cust_key FROM wren.public.orders LIMIT 1",
+        },
+    )
+    assert response.status_code == 200
+    assert (
+        response.text
+        == "\"SELECT orders.orderkey, orders.order_cust_key FROM (SELECT orders.order_cust_key, orders.orderkey FROM (SELECT CONCAT(orders.o_orderkey, '_', orders.o_custkey) AS order_cust_key, orders.o_orderkey AS orderkey FROM public.orders) AS orders) AS orders LIMIT 1\""
+    )
+
+
+def to_connection_info(pg: PostgresContainer):
+    return {
+        "host": pg.get_container_host_ip(),
+        "port": pg.get_exposed_port(pg.port),
+        "user": pg.username,
+        "password": pg.password,
+        "database": pg.dbname,
+    }
+
+
+def to_connection_url(pg: PostgresContainer):
+    info = to_connection_info(pg)
+    return f"postgres://{info['user']}:{info['password']}@{info['host']}:{info['port']}/{info['database']}"
