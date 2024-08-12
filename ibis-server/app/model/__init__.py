3f424225a72537fb4addc4e36bffa2884b2ed27a
Grieve <grieve54706@gmail.com>
2024-08-12 19:28:43 +0800

    fix(mssql): use msodbcsql instead of tdsodbc

--- ibis-server/app/model/__init__.py
+++ ibis-server/app/model/__init__.py
@@ -65,10 +65,8 @@ class MSSqlConnectionInfo(BaseModel):
     database: SecretStr
     user: SecretStr
     password: SecretStr
-    driver: str = Field(
-        default="FreeTDS",
-        description="On Mac and Linux this is usually `FreeTDS. On Windows, it is usually `ODBC Driver 18 for SQL Server`",
-    )
+    driver: str = Field(default="ODBC Driver 18 for SQL Server")
+    kwargs: dict[str, str] | None = None
 
 
 class MySqlConnectionInfo(BaseModel):
--- ibis-server/tests/routers/v2/connector/test_mssql.py
+++ ibis-server/tests/routers/v2/connector/test_mssql.py
@@ -75,7 +75,9 @@ def mssql(request) -> SqlServerContainer:
     mssql = SqlServerContainer(
         "mcr.microsoft.com/mssql/server:2019-CU27-ubuntu-20.04", dialect="mssql+pyodbc"
     ).start()
-    engine = sqlalchemy.create_engine(f"{mssql.get_connection_url()}?driver=FreeTDS")
+    engine = sqlalchemy.create_engine(
+        f"{mssql.get_connection_url()}?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=YES"
+    )
     pd.read_parquet(file_path("resource/tpch/data/orders.parquet")).to_sql(
         "orders", engine, index=False
     )
@@ -387,6 +389,7 @@ def to_connection_info(mssql: SqlServerContainer):
         "user": mssql.username,
         "password": mssql.password,
         "database": mssql.dbname,
+        "kwargs": {"TrustServerCertificate": "YES"},
     }
 
 
