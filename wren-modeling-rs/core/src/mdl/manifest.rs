91e58e96398c7dc0c761b03ba53a557fc7e134cb
Jia-Xuan Liu <liugs963@gmail.com>
2024-08-20 10:32:40 +0800

    use `SCREAMING_SNAKE_CASE` to handle the serde of join type

--- wren-modeling-rs/core/src/mdl/manifest.rs
+++ wren-modeling-rs/core/src/mdl/manifest.rs
@@ -134,13 +134,16 @@ pub struct Relationship {
     pub properties: Vec<(String, String)>,
 }
 
-// handle case insensitive
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Hash, Clone, Copy)]
-#[serde(rename_all = "snake_case")]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum JoinType {
+    #[serde(alias = "one_to_one")]
     OneToOne,
+    #[serde(alias = "one_to_many")]
     OneToMany,
+    #[serde(alias = "many_to_one")]
     ManyToOne,
+    #[serde(alias = "many_to_many")]
     ManyToMany,
 }
 
