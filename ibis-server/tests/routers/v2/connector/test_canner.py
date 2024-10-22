e45b1e7cb46cba6fea2ee395efccff846110f453
Grieve <grieve54706@gmail.com>
2024-10-22 14:23:34 +0800

    test(canner): adjust the pattern

--- ibis-server/tests/routers/v2/connector/test_canner.py
+++ ibis-server/tests/routers/v2/connector/test_canner.py
@@ -20,8 +20,6 @@ The Canner Enterprise must setup below:
 
 pytestmark = pytest.mark.canner
 
-client = TestClient(app)
-
 base_url = "/v2/connector/canner"
 
 connection_info = {
@@ -79,287 +77,279 @@ manifest = {
     ],
 }
 
-manifest_str = base64.b64encode(orjson.dumps(manifest)).decode("utf-8")
+
+@pytest.fixture
+def manifest_str():
+    return base64.b64encode(orjson.dumps(manifest)).decode("utf-8")
 
 
-def test_query():
-    response = client.post(
-        url=f"{base_url}/query",
-        json={
-            "connectionInfo": connection_info,
-            "manifestStr": manifest_str,
-            "sql": 'SELECT * FROM "Orders" ORDER BY orderkey LIMIT 1',
-        },
-    )
-    assert response.status_code == 200, response.text
-    result = response.json()
-    assert len(result["columns"]) == len(manifest["models"][0]["columns"])
-    assert len(result["data"]) == 1
-    assert result["data"][0] == [
-        1,
-        370,
-        "O",
-        172799.49,
-        "1996-01-02",
-        "1_370",
-        "2024-01-01 23:59:59.000000",
-        "2024-01-01 23:59:59.000000 UTC",
-        None,
-    ]
-    assert result["dtypes"] == {
-        "orderkey": "int64",
-        "custkey": "int64",
-        "orderstatus": "object",
-        "totalprice": "float64",
-        "orderdate": "object",
-        "order_cust_key": "object",
-        "timestamp": "object",
-        "timestamptz": "object",
-        "test_null_time": "datetime64[ns]",
-    }
+with TestClient(app) as client:
 
+    def test_query(manifest_str):
+        response = client.post(
+            url=f"{base_url}/query",
+            json={
+                "connectionInfo": connection_info,
+                "manifestStr": manifest_str,
+                "sql": 'SELECT * FROM "Orders" ORDER BY orderkey LIMIT 1',
+            },
+        )
+        assert response.status_code == 200, response.text
+        result = response.json()
+        assert len(result["columns"]) == len(manifest["models"][0]["columns"])
+        assert len(result["data"]) == 1
+        assert result["data"][0] == [
+            1,
+            370,
+            "O",
+            172799.49,
+            "1996-01-02",
+            "1_370",
+            "2024-01-01 23:59:59.000000",
+            "2024-01-01 23:59:59.000000 UTC",
+            None,
+        ]
+        assert result["dtypes"] == {
+            "orderkey": "int64",
+            "custkey": "int64",
+            "orderstatus": "object",
+            "totalprice": "float64",
+            "orderdate": "object",
+            "order_cust_key": "object",
+            "timestamp": "object",
+            "timestamptz": "object",
+            "test_null_time": "datetime64[ns]",
+        }
 
-def test_query_with_connection_url():
-    response = client.post(
-        url=f"{base_url}/query",
-        json={
-            "connectionInfo": {"connectionUrl": _to_connection_url()},
-            "manifestStr": manifest_str,
-            "sql": 'SELECT * FROM "Orders" LIMIT 1',
-        },
-    )
-    assert response.status_code == 200, response.text
-    result = response.json()
-    assert len(result["columns"]) == len(manifest["models"][0]["columns"])
-    assert len(result["data"]) == 1
-    assert result["dtypes"] is not None
+    def test_query_with_connection_url(manifest_str):
+        response = client.post(
+            url=f"{base_url}/query",
+            json={
+                "connectionInfo": {"connectionUrl": _to_connection_url()},
+                "manifestStr": manifest_str,
+                "sql": 'SELECT * FROM "Orders" LIMIT 1',
+            },
+        )
+        assert response.status_code == 200, response.text
+        result = response.json()
+        assert len(result["columns"]) == len(manifest["models"][0]["columns"])
+        assert len(result["data"]) == 1
+        assert result["dtypes"] is not None
 
+    def test_query_with_limit(manifest_str):
+        response = client.post(
+            url=f"{base_url}/query",
+            params={"limit": 1},
+            json={
+                "connectionInfo": connection_info,
+                "manifestStr": manifest_str,
+                "sql": 'SELECT * FROM "Orders"',
+            },
+        )
+        assert response.status_code == 200
+        result = response.json()
+        assert len(result["data"]) == 1
 
-def test_query_with_limit():
-    response = client.post(
-        url=f"{base_url}/query",
-        params={"limit": 1},
-        json={
-            "connectionInfo": connection_info,
-            "manifestStr": manifest_str,
-            "sql": 'SELECT * FROM "Orders"',
-        },
-    )
-    assert response.status_code == 200
-    result = response.json()
-    assert len(result["data"]) == 1
+        response = client.post(
+            url=f"{base_url}/query",
+            params={"limit": 1},
+            json={
+                "connectionInfo": connection_info,
+                "manifestStr": manifest_str,
+                "sql": 'SELECT * FROM "Orders" LIMIT 10',
+            },
+        )
+        assert response.status_code == 200
+        result = response.json()
+        assert len(result["data"]) == 1
 
-    response = client.post(
-        url=f"{base_url}/query",
-        params={"limit": 1},
-        json={
-            "connectionInfo": connection_info,
-            "manifestStr": manifest_str,
-            "sql": 'SELECT * FROM "Orders" LIMIT 10',
-        },
-    )
-    assert response.status_code == 200
-    result = response.json()
-    assert len(result["data"]) == 1
+    def test_query_without_manifest():
+        response = client.post(
+            url=f"{base_url}/query",
+            json={
+                "connectionInfo": connection_info,
+                "sql": 'SELECT * FROM "Orders" LIMIT 1',
+            },
+        )
+        assert response.status_code == 422
+        result = response.json()
+        assert result["detail"][0] is not None
+        assert result["detail"][0]["type"] == "missing"
+        assert result["detail"][0]["loc"] == ["body", "manifestStr"]
+        assert result["detail"][0]["msg"] == "Field required"
 
+    def test_query_without_sql(manifest_str):
+        response = client.post(
+            url=f"{base_url}/query",
+            json={"connectionInfo": connection_info, "manifestStr": manifest_str},
+        )
+        assert response.status_code == 422
+        result = response.json()
+        assert result["detail"][0] is not None
+        assert result["detail"][0]["type"] == "missing"
+        assert result["detail"][0]["loc"] == ["body", "sql"]
+        assert result["detail"][0]["msg"] == "Field required"
 
-def test_query_without_manifest():
-    response = client.post(
-        url=f"{base_url}/query",
-        json={
-            "connectionInfo": connection_info,
-            "sql": 'SELECT * FROM "Orders" LIMIT 1',
-        },
-    )
-    assert response.status_code == 422
-    result = response.json()
-    assert result["detail"][0] is not None
-    assert result["detail"][0]["type"] == "missing"
-    assert result["detail"][0]["loc"] == ["body", "manifestStr"]
-    assert result["detail"][0]["msg"] == "Field required"
+    def test_query_without_connection_info(manifest_str):
+        response = client.post(
+            url=f"{base_url}/query",
+            json={
+                "manifestStr": manifest_str,
+                "sql": 'SELECT * FROM "Orders" LIMIT 1',
+            },
+        )
+        assert response.status_code == 422
+        result = response.json()
+        assert result["detail"][0] is not None
+        assert result["detail"][0]["type"] == "missing"
+        assert result["detail"][0]["loc"] == ["body", "connectionInfo"]
+        assert result["detail"][0]["msg"] == "Field required"
 
+    def test_query_with_dry_run(manifest_str):
+        response = client.post(
+            url=f"{base_url}/query",
+            params={"dryRun": True},
+            json={
+                "connectionInfo": connection_info,
+                "manifestStr": manifest_str,
+                "sql": 'SELECT * FROM "Orders" LIMIT 1',
+            },
+        )
+        assert response.status_code == 204, response.text
 
-def test_query_without_sql():
-    response = client.post(
-        url=f"{base_url}/query",
-        json={"connectionInfo": connection_info, "manifestStr": manifest_str},
-    )
-    assert response.status_code == 422
-    result = response.json()
-    assert result["detail"][0] is not None
-    assert result["detail"][0]["type"] == "missing"
-    assert result["detail"][0]["loc"] == ["body", "sql"]
-    assert result["detail"][0]["msg"] == "Field required"
+    def test_query_with_dry_run_and_invalid_sql(manifest_str):
+        response = client.post(
+            url=f"{base_url}/query",
+            params={"dryRun": True},
+            json={
+                "connectionInfo": connection_info,
+                "manifestStr": manifest_str,
+                "sql": "SELECT * FROM X",
+            },
+        )
+        assert response.status_code == 422
+        assert response.text is not None
 
+    def test_validate_with_unknown_rule(manifest_str):
+        response = client.post(
+            url=f"{base_url}/validate/unknown_rule",
+            json={
+                "connectionInfo": connection_info,
+                "manifestStr": manifest_str,
+                "parameters": {"modelName": "Orders", "columnName": "orderkey"},
+            },
+        )
+        assert response.status_code == 422
+        assert (
+            response.text
+            == f"The rule `unknown_rule` is not in the rules, rules: {rules}"
+        )
 
-def test_query_without_connection_info():
-    response = client.post(
-        url=f"{base_url}/query",
-        json={
-            "manifestStr": manifest_str,
-            "sql": 'SELECT * FROM "Orders" LIMIT 1',
-        },
-    )
-    assert response.status_code == 422
-    result = response.json()
-    assert result["detail"][0] is not None
-    assert result["detail"][0]["type"] == "missing"
-    assert result["detail"][0]["loc"] == ["body", "connectionInfo"]
-    assert result["detail"][0]["msg"] == "Field required"
+    def test_validate_rule_column_is_valid(manifest_str):
+        response = client.post(
+            url=f"{base_url}/validate/column_is_valid",
+            json={
+                "connectionInfo": connection_info,
+                "manifestStr": manifest_str,
+                "parameters": {"modelName": "Orders", "columnName": "orderkey"},
+            },
+        )
+        assert response.status_code == 204, response.text
 
+    def test_validate_rule_column_is_valid_with_invalid_parameters(manifest_str):
+        response = client.post(
+            url=f"{base_url}/validate/column_is_valid",
+            json={
+                "connectionInfo": connection_info,
+                "manifestStr": manifest_str,
+                "parameters": {"modelName": "X", "columnName": "orderkey"},
+            },
+        )
+        assert response.status_code == 422
 
-def test_query_with_dry_run():
-    response = client.post(
-        url=f"{base_url}/query",
-        params={"dryRun": True},
-        json={
-            "connectionInfo": connection_info,
-            "manifestStr": manifest_str,
-            "sql": 'SELECT * FROM "Orders" LIMIT 1',
-        },
-    )
-    assert response.status_code == 204, response.text
+        response = client.post(
+            url=f"{base_url}/validate/column_is_valid",
+            json={
+                "connectionInfo": connection_info,
+                "manifestStr": manifest_str,
+                "parameters": {"modelName": "Orders", "columnName": "X"},
+            },
+        )
+        assert response.status_code == 422
 
+    def test_validate_rule_column_is_valid_without_parameters(manifest_str):
+        response = client.post(
+            url=f"{base_url}/validate/column_is_valid",
+            json={"connectionInfo": connection_info, "manifestStr": manifest_str},
+        )
+        assert response.status_code == 422
+        result = response.json()
+        assert result["detail"][0] is not None
+        assert result["detail"][0]["type"] == "missing"
+        assert result["detail"][0]["loc"] == ["body", "parameters"]
+        assert result["detail"][0]["msg"] == "Field required"
 
-def test_query_with_dry_run_and_invalid_sql():
-    response = client.post(
-        url=f"{base_url}/query",
-        params={"dryRun": True},
-        json={
-            "connectionInfo": connection_info,
-            "manifestStr": manifest_str,
-            "sql": "SELECT * FROM X",
-        },
-    )
-    assert response.status_code == 422
-    assert response.text is not None
+    def test_validate_rule_column_is_valid_without_one_parameter(manifest_str):
+        response = client.post(
+            url=f"{base_url}/validate/column_is_valid",
+            json={
+                "connectionInfo": connection_info,
+                "manifestStr": manifest_str,
+                "parameters": {"modelName": "Orders"},
+            },
+        )
+        assert response.status_code == 422
+        assert response.text == "Missing required parameter: `columnName`"
 
+        response = client.post(
+            url=f"{base_url}/validate/column_is_valid",
+            json={
+                "connectionInfo": connection_info,
+                "manifestStr": manifest_str,
+                "parameters": {"columnName": "orderkey"},
+            },
+        )
+        assert response.status_code == 422
+        assert response.text == "Missing required parameter: `modelName`"
 
-def test_validate_with_unknown_rule():
-    response = client.post(
-        url=f"{base_url}/validate/unknown_rule",
-        json={
-            "connectionInfo": connection_info,
-            "manifestStr": manifest_str,
-            "parameters": {"modelName": "Orders", "columnName": "orderkey"},
-        },
-    )
-    assert response.status_code == 422
-    assert (
-        response.text == f"The rule `unknown_rule` is not in the rules, rules: {rules}"
-    )
+    def test_dry_plan(manifest_str):
+        response = client.post(
+            url=f"{base_url}/dry-plan",
+            json={
+                "manifestStr": manifest_str,
+                "sql": 'SELECT orderkey, order_cust_key FROM "Orders" LIMIT 1',
+            },
+        )
+        assert response.status_code == 200
+        assert response.text is not None
 
+    def test_metadata_list_tables():
+        response = client.post(
+            url=f"{base_url}/metadata/tables",
+            json={"connectionInfo": connection_info},
+        )
+        assert response.status_code == 200
+        result = next(filter(lambda x: x["name"] == "orders", response.json()))
+        assert result["name"] == "orders"
+        assert result["primaryKey"] is not None
+        assert result["description"] == "This is a table comment"
+        assert result["properties"]["catalog"] == "canner"
+        assert result["properties"]["schema"] == "tpch_tiny"
+        assert result["properties"]["table"] == "orders"
+        assert len(result["columns"]) == 9
+        comment_column = next(
+            filter(lambda x: x["name"] == "o_comment", result["columns"])
+        )
+        assert comment_column["description"] == "This is a comment"
 
-def test_validate_rule_column_is_valid():
-    response = client.post(
-        url=f"{base_url}/validate/column_is_valid",
-        json={
-            "connectionInfo": connection_info,
-            "manifestStr": manifest_str,
-            "parameters": {"modelName": "Orders", "columnName": "orderkey"},
-        },
-    )
-    assert response.status_code == 204, response.text
+    def test_metadata_list_constraints():
+        response = client.post(
+            url=f"{base_url}/metadata/constraints",
+            json={"connectionInfo": connection_info},
+        )
+        assert response.status_code == 200
+        assert response.json() == []
 
-
-def test_validate_rule_column_is_valid_with_invalid_parameters():
-    response = client.post(
-        url=f"{base_url}/validate/column_is_valid",
-        json={
-            "connectionInfo": connection_info,
-            "manifestStr": manifest_str,
-            "parameters": {"modelName": "X", "columnName": "orderkey"},
-        },
-    )
-    assert response.status_code == 422
-
-    response = client.post(
-        url=f"{base_url}/validate/column_is_valid",
-        json={
-            "connectionInfo": connection_info,
-            "manifestStr": manifest_str,
-            "parameters": {"modelName": "Orders", "columnName": "X"},
-        },
-    )
-    assert response.status_code == 422
-
-
-def test_validate_rule_column_is_valid_without_parameters():
-    response = client.post(
-        url=f"{base_url}/validate/column_is_valid",
-        json={"connectionInfo": connection_info, "manifestStr": manifest_str},
-    )
-    assert response.status_code == 422
-    result = response.json()
-    assert result["detail"][0] is not None
-    assert result["detail"][0]["type"] == "missing"
-    assert result["detail"][0]["loc"] == ["body", "parameters"]
-    assert result["detail"][0]["msg"] == "Field required"
-
-
-def test_validate_rule_column_is_valid_without_one_parameter():
-    response = client.post(
-        url=f"{base_url}/validate/column_is_valid",
-        json={
-            "connectionInfo": connection_info,
-            "manifestStr": manifest_str,
-            "parameters": {"modelName": "Orders"},
-        },
-    )
-    assert response.status_code == 422
-    assert response.text == "Missing required parameter: `columnName`"
-
-    response = client.post(
-        url=f"{base_url}/validate/column_is_valid",
-        json={
-            "connectionInfo": connection_info,
-            "manifestStr": manifest_str,
-            "parameters": {"columnName": "orderkey"},
-        },
-    )
-    assert response.status_code == 422
-    assert response.text == "Missing required parameter: `modelName`"
-
-
-def test_dry_plan():
-    response = client.post(
-        url=f"{base_url}/dry-plan",
-        json={
-            "manifestStr": manifest_str,
-            "sql": 'SELECT orderkey, order_cust_key FROM "Orders" LIMIT 1',
-        },
-    )
-    assert response.status_code == 200
-    assert response.text is not None
-
-
-def test_metadata_list_tables():
-    response = client.post(
-        url=f"{base_url}/metadata/tables",
-        json={"connectionInfo": connection_info},
-    )
-    assert response.status_code == 200
-    result = next(filter(lambda x: x["name"] == "orders", response.json()))
-    assert result["name"] == "orders"
-    assert result["primaryKey"] is not None
-    assert result["description"] == "This is a table comment"
-    assert result["properties"]["catalog"] == "canner"
-    assert result["properties"]["schema"] == "tpch_tiny"
-    assert result["properties"]["table"] == "orders"
-    assert len(result["columns"]) == 9
-    comment_column = next(filter(lambda x: x["name"] == "o_comment", result["columns"]))
-    assert comment_column["description"] == "This is a comment"
-
-
-def test_metadata_list_constraints():
-    response = client.post(
-        url=f"{base_url}/metadata/constraints",
-        json={"connectionInfo": connection_info},
-    )
-    assert response.status_code == 200
-    assert response.json() == []
-
-
-def _to_connection_url():
-    info = connection_info
-    return f"postgres://{info['user']}:{info['pat']}@{info['host']}:{info['port']}/{info['workspace']}"
+    def _to_connection_url():
+        info = connection_info
+        return f"postgres://{info['user']}:{info['pat']}@{info['host']}:{info['port']}/{info['workspace']}"
