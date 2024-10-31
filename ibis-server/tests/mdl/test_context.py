d8786d1b58b9c0cc0105caffdb42286b12a22642
Grieve <grieve54706@gmail.com>
2024-10-31 14:38:05 +0800

    feat(session-context): cache session context (#870)

---
+++ ibis-server/tests/mdl/test_context.py
@@ -0,0 +1,27 @@
+import base64
+
+import orjson
+
+from app.mdl.context import get_session_context
+from tests.confest import file_path
+
+
+def test_cache():
+    manifest = {
+        "catalog": "my_catalog",
+        "schema": "my_schema",
+        "models": [
+            {
+                "name": "Orders",
+                "refSql": "select * from public.orders",
+                "columns": [
+                    {"name": "orderkey", "expression": "o_orderkey", "type": "integer"}
+                ],
+            },
+        ],
+    }
+    manifest_str = base64.b64encode(orjson.dumps(manifest)).decode("utf-8")
+    function_path = file_path("../resources/function_list")
+    session_context_1 = get_session_context(manifest_str, function_path)
+    session_context_2 = get_session_context(manifest_str, function_path)
+    assert session_context_1 is session_context_2
