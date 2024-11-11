c5b7d179266160d46417d30e79802d40e4738dd5
Jax Liu <liugs963@gmail.com>
2024-11-11 10:42:40 +0800

    chore(function-list): lowercase the function name and add missed timestamp functions for bigquery (#894)

--- ibis-server/tests/routers/v3/connector/snowflake/test_functions.py
+++ ibis-server/tests/routers/v3/connector/snowflake/test_functions.py
@@ -57,15 +57,15 @@ with TestClient(app) as client:
         response = client.get(url=f"{base_url}/functions")
         assert response.status_code == 200
         result = response.json()
-        assert len(result) == 427
+        assert len(result) == 340
         the_func = next(filter(lambda x: x["name"] == "abs", result))
         assert the_func == {
             "name": "abs",
-            "description": "Returns absolute value of the argument",
+            "description": "Returns absolute value",
             "function_type": "scalar",
             "param_names": None,
             "param_types": None,
-            "return_type": "double",
+            "return_type": "number",
         }
 
         config.set_remote_function_list_path(None)
