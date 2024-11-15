2ea1dca6c3fbb2764d198f6b54f4997b2b186e1f
Grieve <grieve54706@gmail.com>
2024-11-15 09:20:57 +0800

    chore(ibis): move the wren_core into function (#911)

--- ibis-server/app/mdl/context.py
+++ ibis-server/app/mdl/context.py
@@ -1,8 +1,8 @@
 from functools import cache
 
-from wren_core import SessionContext
-
 
 @cache
-def get_session_context(manifest_str: str, function_path: str) -> SessionContext:
+def get_session_context(manifest_str: str, function_path: str):
+    from wren_core import SessionContext
+
     return SessionContext(manifest_str, function_path)
