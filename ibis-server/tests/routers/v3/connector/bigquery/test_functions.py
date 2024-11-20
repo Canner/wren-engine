95b5b029e3a90b62ea67a418a6a6062fc25cbdce
Grieve <grieve54706@gmail.com>
2024-11-20 14:41:23 +0800

    chore: fix count

--- ibis-server/tests/routers/v3/connector/bigquery/test_functions.py
+++ ibis-server/tests/routers/v3/connector/bigquery/test_functions.py
@@ -51,7 +51,7 @@ with TestClient(app) as client:
         response = client.get(url=f"{base_url}/functions")
         assert response.status_code == 200
         result = response.json()
-        assert len(result) == DATAFUSION_FUNCTION_COUNT + 22
+        assert len(result) == DATAFUSION_FUNCTION_COUNT + 36
         the_func = next(filter(lambda x: x["name"] == "string_agg", result))
         assert the_func == {
             "name": "string_agg",
