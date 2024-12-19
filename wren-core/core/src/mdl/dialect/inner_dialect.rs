99c367987ab92aaa763a6aefbd2b3026e2fb0570
Jia-Xuan Liu <liugs963@gmail.com>
2024-12-19 18:00:23 +0800

    implement dialect specific function unparsing

---
+++ wren-core/core/src/mdl/dialect/inner_dialect.rs
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+use crate::mdl::dialect::utils::scalar_function_to_sql_internal;
+use crate::mdl::manifest::DataSource;
+use datafusion::common::Result;
+use datafusion::logical_expr::Expr;
+use datafusion::sql::sqlparser::ast;
+use datafusion::sql::unparser::Unparser;
+
+pub trait InnerDialect: Send + Sync {
+    fn scalar_function_to_sql_overrides(
+        &self,
+        _unparser: &Unparser,
+        _function_name: &str,
+        _args: &[Expr],
+    ) -> Result<Option<ast::Expr>> {
+        Ok(None)
+    }
+}
+
+pub fn get_inner_dialect(data_source: &DataSource) -> Box<dyn InnerDialect> {
+   match data_source {
+        DataSource::MySQL => Box::new(MySQLDialect {}),
+        _ => Box::new(GenericDialect {}),
+    }
+}
+
+pub struct GenericDialect {}
+
+impl InnerDialect for GenericDialect {}
+
+pub struct MySQLDialect {}
+
+impl InnerDialect for MySQLDialect {
+    fn scalar_function_to_sql_overrides(
+        &self,
+        unparser: &Unparser,
+        function_name: &str,
+        args: &[Expr],
+    ) -> Result<Option<ast::Expr>> {
+        match function_name {
+            "btrim" => scalar_function_to_sql_internal(unparser, "trim", args),
+            _ => Ok(None),
+        }
+    }
+}
