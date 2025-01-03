b33d203fadfdc7c6576956575b693d8c4f6a1ceb
Jax Liu <liugs963@gmail.com>
2025-01-03 18:39:31 +0800

    change fmt

--- ibis-server/app/util.py
+++ ibis-server/app/util.py
@@ -29,7 +29,7 @@ def _to_datetime_and_format(series: pd.Series) -> pd.Series:
 
 
 def _to_json_obj(df: pd.DataFrame) -> dict:
-    data = df.map(lambda x: f"{x:.9f}" if isinstance(x, float) else x).to_dict(
+    data = df.map(lambda x: f"{x:.9g}" if isinstance(x, float) else x).to_dict(
         orient="split", index=False
     )
 
