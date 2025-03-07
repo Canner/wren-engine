cca909bc4a033620cbcc6bae9b2a72372c4aa413
Jax Liu <liugs963@gmail.com>
2025-03-07 16:42:38 +0800

    implement limit pushdown

--- wren-core/core/src/lib.rs
+++ wren-core/core/src/lib.rs
@@ -4,4 +4,5 @@ pub mod mdl;
 pub use datafusion::error::DataFusionError;
 pub use datafusion::logical_expr::{AggregateUDF, ScalarUDF, WindowUDF};
 pub use datafusion::prelude::SessionContext;
+pub use datafusion::sql::sqlparser::*;
 pub use mdl::AnalyzedWrenMDL;
