9da7ff1a1dfed3fee80ec591d3fa1695bbca1db7
Jax Liu <liugs963@gmail.com>
2025-07-15 11:24:34 +0800

    merge docker image

--- ibis-server/entrypoint.sh
+++ ibis-server/entrypoint.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+# Check if the first argument is 'jupyter'
+if [[ "$1" == "jupyter" ]]; then
+    # If only 'jupyter' is passed, start jupyter lab with default configuration
+    if [[ $# -eq 1 ]]; then
+        echo "Starting Jupyter Lab..."
+        exec jupyter lab \
+            --ip=0.0.0.0 \
+            --port=8888 \
+            --no-browser \
+            --allow-root \
+            --ServerApp.token="" \
+            --ServerApp.password="" \
+            --ServerApp.allow_origin="*" \
+            --ServerApp.base_url="/lab" \
+            --ServerApp.default_url="/lab/tree/notebooks/demo.ipynb" \
+            --ServerApp.iopub_data_rate_limit=1000000000 \
+            --ServerApp.websocket_ping_timeout=30000 \
+            --ServerApp.websocket_ping_interval=30000
+    else
+        # If jupyter with additional arguments, pass all arguments to jupyter
+        echo "Executing: $@"
+        exec "$@"
+    fi
+fi
+
+# Default behavior: start ibis-server
+echo "Starting Ibis Server..."
+
 if [[ -z "${WREN_NUM_WORKERS}" ]]; then
     echo "WREN_NUM_WORKERS is not set. Using default value of 2."
     WREN_NUM_WORKERS=2
