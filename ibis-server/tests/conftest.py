32c0bdfdf5d1d8bd4b1c04a051f1498f66d38d81
Jax Liu <liugs963@gmail.com>
2025-08-22 10:22:12 +0800

    update number of datafusion

--- ibis-server/tests/conftest.py
+++ ibis-server/tests/conftest.py
@@ -11,7 +11,7 @@ def file_path(path: str) -> str:
     return os.path.join(os.path.dirname(__file__), path)
 
 
-DATAFUSION_FUNCTION_COUNT = 276
+DATAFUSION_FUNCTION_COUNT = 283
 
 
 @pytest.fixture(scope="session")
