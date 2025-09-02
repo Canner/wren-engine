ab1e15f7d6ecbbff95b02338bc72dc24f92922ed
Jax Liu <liugs963@gmail.com>
2025-09-02 12:05:48 +0800

    fix test

--- wren-core-py/tests/test_modeling_core.py
+++ wren-core-py/tests/test_modeling_core.py
@@ -137,7 +137,7 @@ def test_read_function_list():
     path = "tests/functions.csv"
     session_context = SessionContext(manifest_str, path)
     functions = session_context.get_available_functions()
-    assert len(functions) == 290
+    assert len(functions) == 292
 
     rewritten_sql = session_context.transform_sql(
         "SELECT add_two(c_custkey, c_custkey) FROM my_catalog.my_schema.customer"
@@ -149,7 +149,7 @@ def test_read_function_list():
 
     session_context = SessionContext(manifest_str, None)
     functions = session_context.get_available_functions()
-    assert len(functions) == 283
+    assert len(functions) == 285
 
 
 def test_get_available_functions():
