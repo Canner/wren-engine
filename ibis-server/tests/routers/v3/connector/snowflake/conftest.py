26b2bf51d11e82d03e0c96fec9ddb1889e089159
Jax Liu <liugs963@gmail.com>
2025-10-29 13:38:57 +0800

    address comment

--- ibis-server/tests/routers/v3/connector/snowflake/conftest.py
+++ ibis-server/tests/routers/v3/connector/snowflake/conftest.py
@@ -16,67 +16,75 @@ def pytest_collection_modifyitems(items):
             item.add_marker(pytestmark)
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope="module", autouse=True)
 def init_snowflake():
+    user = os.getenv("SNOWFLAKE_USER")
+    account = os.getenv("SNOWFLAKE_ACCOUNT")
+    private_key = os.getenv("SNOWFLAKE_PRIVATE_KEY")
+    if not user or not account or not private_key:
+        pytest.skip("Snowflake credentials are not set", allow_module_level=True)
+
     conn = snowflake.connector.connect(
-        user=os.getenv("SNOWFLAKE_USER"),
-        account=os.getenv("SNOWFLAKE_ACCOUNT"),
-        private_key=os.getenv("SNOWFLAKE_PRIVATE_KEY"),
+        user=user,
+        account=account,
+        private_key=private_key,
         warehouse="COMPUTE_WH",
     )
-    cs = conn.cursor()
     try:
-        cs.execute("USE WREN")
-    except Exception:
-        cs.execute("CREATE DATABASE IF NOT EXISTS WREN")
-        cs.execute("USE WREN")
-    try:
-        cs.execute("USE SCHEMA PUBLIC")
-    except Exception:
-        cs.execute("CREATE SCHEMA IF NOT EXISTS PUBLIC")
-        cs.execute("USE SCHEMA PUBLIC")
-    # prepare table with variant column
-    cs.execute(
-        """
-        CREATE OR REPLACE TABLE car_sales
-        ( 
-        src variant
+        cs = conn.cursor()
+        try:
+            cs.execute("USE WREN")
+        except Exception:
+            cs.execute("CREATE DATABASE IF NOT EXISTS WREN")
+            cs.execute("USE WREN")
+        try:
+            cs.execute("USE SCHEMA PUBLIC")
+        except Exception:
+            cs.execute("CREATE SCHEMA IF NOT EXISTS PUBLIC")
+            cs.execute("USE SCHEMA PUBLIC")
+        # prepare table with variant column
+        cs.execute(
+            """
+            CREATE OR REPLACE TABLE car_sales
+            ( 
+            src variant
+            )
+            AS
+            SELECT PARSE_JSON(column1) AS src
+            FROM VALUES
+            ('{ 
+                "date" : "2017-04-28", 
+                "dealership" : "Valley View Auto Sales",
+                "salesperson" : {
+                "id": "55",
+                "name": "Frank Beasley"
+                },
+                "customer" : [
+                {"name": "Joyce Ridgely", "phone": "16504378889", "address": "San Francisco, CA"}
+                ],
+                "vehicle" : [
+                {"make": "Honda", "model": "Civic", "year": "2017", "price": "20275", "extras":["ext warranty", "paint protection"]}
+                ]
+            }'),
+            ('{ 
+                "date" : "2017-04-28", 
+                "dealership" : "Tindel Toyota",
+                "salesperson" : {
+                "id": "274",
+                "name": "Greg Northrup"
+                },
+                "customer" : [
+                {"name": "Bradley Greenbloom", "phone": "12127593751", "address": "New York, NY"}
+                ],
+                "vehicle" : [
+                {"make": "Toyota", "model": "Camry", "year": "2017", "price": "23500", "extras":["ext warranty", "rust proofing", "fabric protection"]}  
+                ]
+            }') v;
+            """
         )
-        AS
-        SELECT PARSE_JSON(column1) AS src
-        FROM VALUES
-        ('{ 
-            "date" : "2017-04-28", 
-            "dealership" : "Valley View Auto Sales",
-            "salesperson" : {
-            "id": "55",
-            "name": "Frank Beasley"
-            },
-            "customer" : [
-            {"name": "Joyce Ridgely", "phone": "16504378889", "address": "San Francisco, CA"}
-            ],
-            "vehicle" : [
-            {"make": "Honda", "model": "Civic", "year": "2017", "price": "20275", "extras":["ext warranty", "paint protection"]}
-            ]
-        }'),
-        ('{ 
-            "date" : "2017-04-28", 
-            "dealership" : "Tindel Toyota",
-            "salesperson" : {
-            "id": "274",
-            "name": "Greg Northrup"
-            },
-            "customer" : [
-            {"name": "Bradley Greenbloom", "phone": "12127593751", "address": "New York, NY"}
-            ],
-            "vehicle" : [
-            {"make": "Toyota", "model": "Camry", "year": "2017", "price": "23500", "extras":["ext warranty", "rust proofing", "fabric protection"]}  
-            ]
-        }') v;
-        """
-    )
-    cs.close()
-    conn.close()
+    finally:
+        cs.close()
+        conn.close()
 
 
 @pytest.fixture(scope="module")
