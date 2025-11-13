efee7ffb522843775905acc21292c2dddd893c8e
Jax Liu <liugs963@gmail.com>
2025-11-13 14:46:55 +0800

    merge tests

--- ibis-server/tests/routers/v3/connector/oracle/test_query.py
+++ ibis-server/tests/routers/v3/connector/oracle/test_query.py
@@ -3,8 +3,10 @@ import base64
 import orjson
 import pytest
 
+from app.config import get_config
 from app.dependencies import X_WREN_FALLBACK_DISABLE
-from tests.routers.v3.connector.oracle.conftest import base_url
+from tests.conftest import DATAFUSION_FUNCTION_COUNT
+from tests.routers.v3.connector.oracle.conftest import base_url, function_list_path
 
 manifest = {
     "catalog": "my_catalog",
@@ -258,3 +260,76 @@ async def test_order_by_nulls_last(client, manifest_str, connection_info):
     assert result["data"][0][0] == "two"
     assert result["data"][1][0] == "one"
     assert result["data"][2][0] == "three"
+
+
+async def test_function_list(client):
+    config = get_config()
+
+    config.set_remote_function_list_path(None)
+    response = await client.get(url=f"{base_url}/functions")
+    assert response.status_code == 200
+    result = response.json()
+    assert len(result) == DATAFUSION_FUNCTION_COUNT
+
+    config.set_remote_function_list_path(function_list_path)
+    response = await client.get(url=f"{base_url}/functions")
+    assert response.status_code == 200
+    result = response.json()
+    assert len(result) == DATAFUSION_FUNCTION_COUNT + 1
+    the_func = next(filter(lambda x: x["name"] == "to_timestamp_tz", result))
+    assert the_func == {
+        "name": "to_timestamp_tz",
+        "description": "Convert string to timestamp with time zone",
+        "function_type": "scalar",
+        "param_names": None,
+        "param_types": None,
+        "return_type": None,
+    }
+
+    config.set_remote_function_list_path(None)
+    response = await client.get(url=f"{base_url}/functions")
+    assert response.status_code == 200
+    result = response.json()
+    assert len(result) == DATAFUSION_FUNCTION_COUNT
+
+
+async def test_scalar_function(client, manifest_str: str, connection_info):
+    response = await client.post(
+        url=f"{base_url}/query",
+        json={
+            "connectionInfo": connection_info,
+            "manifestStr": manifest_str,
+            "sql": "SELECT ABS(-1) AS col",
+        },
+        headers={
+            X_WREN_FALLBACK_DISABLE: "true",
+        },
+    )
+    assert response.status_code == 200
+    result = response.json()
+    assert result == {
+        "columns": ["col"],
+        "data": [[1]],
+        "dtypes": {"col": "int64"},
+    }
+
+
+async def test_aggregate_function(client, manifest_str: str, connection_info):
+    response = await client.post(
+        url=f"{base_url}/query",
+        json={
+            "connectionInfo": connection_info,
+            "manifestStr": manifest_str,
+            "sql": "SELECT COUNT(*) AS col FROM (SELECT 1) AS temp_table",
+        },
+        headers={
+            X_WREN_FALLBACK_DISABLE: "true",
+        },
+    )
+    assert response.status_code == 200
+    result = response.json()
+    assert result == {
+        "columns": ["col"],
+        "data": [[1]],
+        "dtypes": {"col": "int64"},
+    }
