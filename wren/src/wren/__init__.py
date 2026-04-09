1875593fc92a81c95c6060c566dcf8e398de60c8
Jax Liu <liugs963@gmail.com>
2026-04-09 18:01:11 +0800

    chore: prepare 0.2.1

--- wren/src/wren/__init__.py
+++ wren/src/wren/__init__.py
@@ -1,6 +1,6 @@
 """Wren — semantic SQL layer for 20+ data sources."""
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 from wren.engine import WrenEngine
 from wren.model.data_source import DataSource
